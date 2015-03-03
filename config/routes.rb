689944e312925affbafbf0da7e3df8b8c54f9264
qinmingyuan <mingyuan0715@foxmail.com>
2015-03-03 20:30:20 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,32 @@
 Rails.application.routes.draw do
+  namespace :admin do
+
+    resources :goods do
+      get 'pic' => :edit_pic, :on => :member
+      patch 'pic' => :update_pic, :on => :member
+      get 'items' => :edit_items, :on => :member
+      patch 'items' => :update_items, :on => :member
+      get 'taxons' => :edit_taxons, :on => :member
+      patch 'taxons' => :update_taxons, :on => :member
+      get 'promote' => :edit_promote, :on => :member
+      patch 'promote' => :update_promote, :on => :member
+      resources :good_items
+      resources :sales
+    end
+
+
+    resources :users, :only => [:index, :show, :destroy]
+    resources :invites
+    resources :races do
+      resources :crowds, :shallow => true
+    end
+    resources :taxons
+    resources :promotes
+    resources :providers
+    resources :produces
+    resources :orders, :only => [:index, :show, :edit, :update, :destroy]
+    resources :carts, :only => [:index, :destroy]
+    resources :areas
+
+  end
 end
