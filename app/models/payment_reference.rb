d047d49abbadca5ad12a532abaf5bcebd786c053
qinmingyuan <mingyuan0715@foxmail.com>
2017-06-07 11:19:36 +0800

    association before add does not use activesupport callbacks

--- app/models/payment_reference.rb
+++ app/models/payment_reference.rb
@@ -2,4 +2,12 @@ class PaymentReference < ApplicationRecord
   belongs_to :payment_method, autosave: true, inverse_of: :payment_references
   belongs_to :buyer, autosave: true, inverse_of: :payment_references
 
+  before_save :prevent_duplicate
+
+  def prevent_duplicate
+    if PaymentReference.exists?(payment_method_id: self.payment_method_id, buyer_id: self.buyer_id)
+      throw(:abort)
+    end
+  end
+
 end
