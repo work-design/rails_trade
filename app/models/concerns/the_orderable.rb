799a84f8e22aa3ff6cbc03003cc1d6c7db25e385
qinmingyuan <mingyuan0715@foxmail.com>
2017-06-20 10:20:57 +0800

    fix order able

--- app/models/concerns/the_orderable.rb
+++ app/models/concerns/the_orderable.rb
@@ -1,19 +1,14 @@
-module TheBuyable
+module TheOrderable
   extend ActiveSupport::Concern
 
   included do
-    belongs_to :buyer, optional: true, autosave: true
+    belongs_to :buyer, optional: true
+    has_many :payment_orders, dependent: :destroy
+    has_many :payments, through: :payment_orders
   end
 
-  def get_buyer
-    if buyer
-      return buyer
-    else
-      build_buyer(name: self.name)
-      save
-    end
-
-    buyer
+  def unreceived_amount
+    self.amount - self.received_amount
   end
 
 end
