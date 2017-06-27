0a86d8e59771d73dccef0d7195301fb2d23b529e
qinmingyuan <mingyuan0715@foxmail.com>
2017-06-27 10:46:32 +0800

    价格计算逻辑

--- app/models/orders/order_item.rb
+++ app/models/orders/order_item.rb
@@ -2,11 +2,16 @@ class OrderItem < ApplicationRecord
   belongs_to :good
 
 
+  def compute_fee
+    if number.blank? && self.unit.present?
+      self.number = (self.quantity.to_f / good.quantity.to_f).ceil
+    end
 
-
-  def compute_fee(total)
-    number = (total / good.quantity).ceil
-    Charge.price(number, unit)
+    if self.unit
+      self.amount = Charge.price(quantity, self.unit)
+    else
+      self.amount = self.number * good.price
+    end
   end
 
 end
