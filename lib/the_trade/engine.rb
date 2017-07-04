1bf5edeb3b57d5875bd0e6077dc44796a928e46c
qinmingyuan <mingyuan0715@foxmail.com>
2017-07-05 07:31:27 +0800

    ini order items

--- lib/the_trade/engine.rb
+++ lib/the_trade/engine.rb
@@ -2,9 +2,9 @@ module TheTrade
   class Engine < ::Rails::Engine
 
     config.eager_load_paths += Dir["#{config.root}/app/models/payments"]
-    #config.eager_load_paths += Dir["#{config.root}/app/models/orders"]
+    config.eager_load_paths += Dir["#{config.root}/app/models/orders"]
     config.eager_load_paths += Dir["#{config.root}/app/models/charges"]
-    #config.eager_load_paths += Dir["#{config.root}/app/models/refunds"]
+    config.eager_load_paths += Dir["#{config.root}/app/models/refunds"]
     initializer 'the_trade.assets.precompile' do |app|
       app.config.assets.precompile += ['the_trade_manifest.js']
     end
