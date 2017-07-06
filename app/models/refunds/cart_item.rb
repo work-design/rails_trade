0f4d4c4316ff4fc0621966d198ce5480f80ca50a
qinmingyuan <mingyuan0715@foxmail.com>
2017-07-06 11:17:56 +0800

    comment code

--- app/models/refunds/cart_item.rb
+++ app/models/refunds/cart_item.rb
@@ -1,11 +1,11 @@
 class CartItem < ApplicationRecord
   belongs_to :good, polymorphic: true
 
-  enum status: [
-    :unpaid,
-    :deleted,
-    :paid
-  ]
+  # enum status: [
+  #   :unpaid,
+  #   :deleted,
+  #   :paid
+  # ]
 
   scope :valid, -> { default_where(status: 0) }
 
