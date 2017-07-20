b8e3de8930d9ee46627aefbec0a0666019819a54
qinmingyuan <mingyuan0715@foxmail.com>
2017-07-20 16:06:53 +0800

    avoid auth token

--- app/controllers/my/the_trade_controller.rb
+++ app/controllers/my/the_trade_controller.rb
@@ -1,5 +1,5 @@
 class My::TheTradeController < TheTrade.config.my_class.constantize
-  skip_before_action :verify_authenticity_token, if: -> { request.content_type == 'application/json' }
+  skip_before_action :verify_authenticity_token#, if: -> { request.content_type == 'application/json' }
 
 
   def index
