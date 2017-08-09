8ab999ca81f3882ff32da498793e370343321b34
qinmingyuan <mingyuan0715@foxmail.com>
2017-08-09 10:44:28 +0800

    订单金额字符串

--- app/models/concerns/the_alipay.rb
+++ app/models/concerns/the_alipay.rb
@@ -7,7 +7,7 @@ module TheAlipay
   def create_alipay
     Alipay::Service.trade_app_pay_params subject: self.subject,
                                          out_trade_no: self.uuid,
-                                         total_amount: self.amount
+                                         total_amount: self.amount.to_s
   end
 
   def subject
