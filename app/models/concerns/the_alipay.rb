ea63e957a3c5aa2144d81c2f95202e3c2d1774e7
qinmingyuan <mingyuan0715@foxmail.com>
2017-08-09 13:52:31 +0800

    alipay 对账

--- app/models/concerns/the_alipay.rb
+++ app/models/concerns/the_alipay.rb
@@ -10,6 +10,40 @@ module TheAlipay
                                          total_amount: self.amount.to_s
   end
 
+  def create_alipay_url
+    Alipay::Service.trade_page_pay subject: self.subject, out_trade_no: self.uuid, total_amount: self.amount.to_s
+  end
+
+  # 3 step: check result
+  def alipay_result
+    result = Alipay::Service.trade_query out_trade_no: self.uuid
+    result = JSON.parse(result)
+    result = result['alipay_trade_query_response']
+
+    if result['trade_status'] == 'TRADE_SUCCESS'
+      alipay = AlipayPayment.new
+      alipay.payment_uuid = result['trade_no']
+      alipay.buyer_identifier = result['buyer_user_id']
+      alipay.total_amount = result['total_amount']
+      alipay.income_amount = result['receipt_amount']
+      alipay.notified_at = result['send_pay_date']
+
+      payment_order = alipay.payment_orders.build(order_id: self.id, check_amount: alipay.total_amount)
+
+      begin
+        Payment.transaction do
+          payment_order.confirm!
+          alipay.save!
+        end
+        alipay
+      rescue
+        AlipayPayment.find_by(payment_uuid: result['trade_no'])
+      end
+    else
+      errors.add :uuid, result['msg']
+    end
+  end
+
   def subject
     order_items.map { |oi| oi.good.name }.join(', ')
   end
