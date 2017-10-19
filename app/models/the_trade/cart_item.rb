877f379143005ce685e61cd87dede3a6d9c2ec7a
qinmingyuan <mingyuan0715@foxmail.com>
2017-10-19 16:30:58 +0800

    fix cart bugs;

--- app/models/the_trade/cart_item.rb
+++ app/models/the_trade/cart_item.rb
@@ -1,7 +1,6 @@
 class CartItem < ApplicationRecord
-  belongs_to :good, polymorphic: true
+  belongs_to :good, polymorphic: true, optional: true
   scope :valid, -> { default_where(status: 'unpaid') }
-
 
   enum status: [
     :unpaid,
