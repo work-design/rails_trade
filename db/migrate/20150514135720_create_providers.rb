8eda5501e226207667111eca9837343c0f573002
qinmingyuan <mingyuan0715@foxmail.com>
2017-11-15 22:47:51 +0800

    add area migration

--- db/migrate/20150514135720_create_providers.rb
+++ db/migrate/20150514135720_create_providers.rb
@@ -33,5 +33,15 @@ class CreateProviders < ActiveRecord::Migration[5.1]
       t.timestamps
     end
 
+    create_table :areas do |t|
+      t.string :nation, default: ''
+      t.string :province, default: ''
+      t.string :city, default: ''
+      t.string :district, default: ''
+      t.boolean :published, default: true
+      t.boolean :popular, default: false
+      t.timestamps
+    end
+
   end
 end
