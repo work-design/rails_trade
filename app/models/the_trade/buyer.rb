4b5a547112a8df9022cd7b765bdb64a161d12635
qinmingyuan <mingyuan0715@foxmail.com>
2017-12-11 11:50:37 +0800

    add buyer

--- app/models/the_trade/buyer.rb
+++ app/models/the_trade/buyer.rb
@@ -1,6 +1,6 @@
 class Buyer < ApplicationRecord
   belongs_to :payment_strategy, optional: true
-  has_many :users, dependent: :nullify
+  has_many :users, foreign_key: :buyer_id, dependent: :nullify
   has_many :orders, foreign_key: :buyer_id, inverse_of: :buyer
   has_many :payment_references, foreign_key: :buyer_id, dependent: :destroy, autosave: true
   has_many :payment_methods, through: :payment_references, autosave: true
