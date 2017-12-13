0b06200d685f7a2e977d12a435f101f1f519f697
JonsonHydrap <wangqsh999@gmail.com>
2017-12-13 18:34:38 +0800

    fix creating order without buyer_id

--- app/models/the_trade/concerns/the_stripe.rb
+++ app/models/the_trade/concerns/the_stripe.rb
@@ -11,7 +11,7 @@ module TheStripe
   def stripe_customer(params)
     customer = Stripe::Customer.create(description: "#{buyer_id}", source: params[:token])
 
-    payment_method = customer.payment_methods.build(type: 'StripeMethod')
+    payment_method = buyer.payment_methods.build(type: 'StripeMethod')
     payment_method.account_num = customer.id
     payment_method.extra = payment_method.customer_info(customer)
     payment_method.save
@@ -22,13 +22,19 @@ module TheStripe
       stripe_customer(token: params[:token])
     end
 
-    if params[:payment_method_id]
-      stripe_payment_method = buyer.payment_methods.where(type: 'StripeMethod', id: params[:payment_method_id]).first
+    if params[:payment_method_id].present?
+      stripe_payment_method = buyer.payment_methods
+        .where(type: 'StripeMethod', id: params[:payment_method_id])
+        .first
     else
-      stripe_payment_method = buyer.payment_methods.where(type: 'StripeMethod').first
+      stripe_payment_method = buyer.payment_methods
+        .where(type: 'StripeMethod').first
     end
 
-    charge = Stripe::Charge.create(amount: (self.amount * 100).to_i, currency: self.currency, customer: stripe_payment_method.account_num)
+    charge = Stripe::Charge.create(
+      amount: (self.amount * 100).to_i,
+      currency: self.currency, customer: stripe_payment_method.account_num
+    )
     self.update payment_type: 'stripe', payment_id: charge.id
     self.stripe_record(charge)
   end
