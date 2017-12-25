5e5c35a5509bfab40169b05914141688ad077097
JonsonHydrap <wangqsh999@gmail.com>
2017-12-25 16:16:38 +0800

    remove reductant codes

--- app/models/the_trade/concerns/the_balance.rb
+++ app/models/the_trade/concerns/the_balance.rb
@@ -5,6 +5,13 @@ module TheBalance
     self.update payment_type: 'balance'
   end
 
+  def create_balance_pay(buyer)
+    self.update payment_type: 'balance'
+    BalancePayService.pay(buyer_id: buyer.id,
+                          amount:   self.amount,
+                          order_id: self.id)
+  end
+
   def balance_result
     usable_balances_sum = self.buyer.usable_balances_sum
     if usable_balances_sum < self.total_amount
