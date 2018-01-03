2d1470c752594c3459d02e704eac34fb3aecbe52
qinmingyuan <mingyuan0715@foxmail.com>
2018-01-03 11:45:14 +0800

    fix 已审核 in payment methods page

--- app/controllers/the_trade_admin/payment_methods_controller.rb
+++ app/controllers/the_trade_admin/payment_methods_controller.rb
@@ -1,5 +1,6 @@
 class TheTradeAdmin::PaymentMethodsController < TheTradeAdmin::BaseController
   before_action :set_payment_method, only: [:show, :edit, :update, :verify, :merge_from, :destroy]
+  default_form_builder nil
 
   def index
     @payment_methods = PaymentMethod.includes(:payment_references).default_where(params.permit(:id)).default_where(params.fetch(:q, {}).permit(:account_name, :account_num, :bank)).page(params[:page])
