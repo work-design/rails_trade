5256f6edd83b73311a624507ab785ff9820e9ae6
JonsonHydrap <wangqsh999@gmail.com>
2018-01-03 23:39:50 +0800

    refactor balance refund

--- app/models/the_trade/refunds/balance_refund.rb
+++ app/models/the_trade/refunds/balance_refund.rb
@@ -4,16 +4,15 @@ class BalanceRefund < Refund
 
     order = self.order
     refunded_amount = 0
-    self.class.transaction do
+    # self.class.transaction do
       # 1. Refund to app_user_balances
-      order.app_user_balance_payments
-        .includes(:source, app_user_balance: [:app_user, :balance_rule])
-        .each do |payment|
+       # .includes(:source, app_user_balance: [:app_user, :balance_rule])
+      order.app_user_balance_payments.each do |payment|
         balance = payment.app_user_balance
         available_refund_amount = order.refund_price - refunded_amount
 
-        if available_refund_amount >= balance.price
-          available_refund_amount = balance_price
+        if available_refund_amount >= payment.price
+          available_refund_amount = payment.price
         end
 
         if available_refund_amount < 0
@@ -22,19 +21,18 @@ class BalanceRefund < Refund
 
         balance.update_attributes(
           price:          (balance.price + available_refund_amount),
-          payment_status: false,
           status:         'usable')
 
-        refunded_amount += payment.price
+        refunded_amount += available_refund_amount
         payment.refunded!
 
         break if refunded_amount == order.refund_price
       end
 
       # 2. Update payments table status
-      #order.payments.each do |payment|
-      #  payment.pay_status = 'REFUNDED'
-      #end
+      order.payments.each do |payment|
+        payment.pay_status = 'REFUNDED'
+      end
 
       # 3. Update orders
       order.update_attributes(
@@ -45,7 +43,7 @@ class BalanceRefund < Refund
                 operator_id: params[:operator_id],
                 state:       'completed',
                 refunded_at: Time.now)
-    end
+    # end
   rescue Exception => e
     Rails.logger.fatal e.inspect
     self.update reason: 'failed'
