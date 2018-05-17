959135ac3574acf923c87910d801eb7d14c85ba7
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-17 14:45:02 +0800

    simply code

--- lib/the_trade/engine.rb
+++ lib/the_trade/engine.rb
@@ -2,16 +2,18 @@ require 'monetize'
 module TheTrade
   class Engine < ::Rails::Engine
 
-    config.eager_load_paths += Dir["#{config.root}/app/models/the_trade"]
-    config.eager_load_paths += Dir["#{config.root}/app/models/the_trade/concerns"]
-    config.eager_load_paths += Dir["#{config.root}/app/models/the_trade/payment_interface"]
-    config.eager_load_paths += Dir["#{config.root}/app/models/the_trade/payments"]
-    config.eager_load_paths += Dir["#{config.root}/app/models/the_trade/payment_methods"]
-    config.eager_load_paths += Dir["#{config.root}/app/models/the_trade/refunds"]
-    config.eager_load_paths += Dir["#{config.root}/app/models/the_trade/promotes"]
-    config.eager_load_paths += Dir["#{config.root}/app/models/the_trade/serves"]
-    config.eager_load_paths += Dir["#{config.root}/app/models/the_trade/promote_charges"]
-    config.eager_load_paths += Dir["#{config.root}/app/models/the_trade/serve_charges"]
+    config.eager_load_paths += Dir[
+      "#{config.root}/app/models/the_trade",
+      "#{config.root}/app/models/the_trade/concerns",
+      "#{config.root}/app/models/the_trade/payment_interface",
+      "#{config.root}/app/models/the_trade/payments",
+      "#{config.root}/app/models/the_trade/payment_methods",
+      "#{config.root}/app/models/the_trade/refunds",
+      "#{config.root}/app/models/the_trade/promotes",
+      "#{config.root}/app/models/the_trade/serves",
+      "#{config.root}/app/models/the_trade/promote_charges",
+      "#{config.root}/app/models/the_trade/serve_charges"
+    ]
 
     initializer 'the_trade.assets.precompile' do |app|
       app.config.assets.precompile += ['the_trade_manifest.js']
