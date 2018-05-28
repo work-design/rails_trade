63d098a46d18029e3bf484dc6517766f5be55df3
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-28 10:18:05 +0800

    maybe different buyer class

--- lib/the_trade.rb
+++ lib/the_trade.rb
@@ -8,10 +8,12 @@ module TheTrade
   end
 
   def self.buyer_class=(buyer)
-    if @buyer_class.is_a?(Class)
-      raise 'You can not include TheTradeBuyer more than once'
+    if @buyer_class.name == buyer.name
+      return
     elsif buyer.ancestors.include?(ActiveRecord::Base)
       @buyer_class = buyer
+    elsif buyer.is_a?(Class)
+      raise 'You can not include TheTradeBuyer more than once'
     else
       raise 'You must assign an ActiveRecord class'
     end
