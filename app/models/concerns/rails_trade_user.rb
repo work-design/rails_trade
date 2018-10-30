56cc7a88ea057afd209a8590568a75fe4c6409c2
qinmingyuan <mingyuan0715@foxmail.com>
2018-10-30 09:16:24 +0800

    remove provider

--- app/models/concerns/rails_trade_user.rb
+++ app/models/concerns/rails_trade_user.rb
@@ -4,8 +4,6 @@ module RailsTradeUser
 
   included do
     attribute :buyer_type, :string, default: self.name
-    attribute :provider_id, :integer
-    belongs_to :provider, inverse_of: :users, autosave: true, optional: true
   end
 
 end
