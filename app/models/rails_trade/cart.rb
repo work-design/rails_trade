6abc42bda1bbd1a15378f227c79fdf39a4c7c348
qinmingyuan <mingyuan0715@foxmail.com>
2019-01-07 19:46:51 +0800

    update config

--- app/models/rails_trade/cart.rb
+++ app/models/rails_trade/cart.rb
@@ -1,12 +1,11 @@
 # 数据定期清理
 # 改变数据动作：
-#   * 新增
+#   * 新增(check)
 #   * 更新数量
-#   * check
 #   * 选择或更换优惠券
 #   * 选择服务
 class Cart < ApplicationRecord
   has_many :cart_serves, -> { includes(:serve) }, dependent: :destroy
-  has_many :cart_promotes, dependent: :destroy
+  has_many :cart_promotes, -> { includes(:promote) }, dependent: :destroy
 
 end
