4dda4f12660a6849538e7ae0d680596c063ca408
qinmingyuan <mingyuan0715@foxmail.com>
2019-01-08 17:47:23 +0800

    fix callback

--- app/models/rails_trade/order_promote.rb
+++ app/models/rails_trade/order_promote.rb
@@ -7,5 +7,11 @@ class OrderPromote < ApplicationRecord
   belongs_to :promote
   belongs_to :promote_charge, optional: true
   belongs_to :promote_buyer, optional: true, counter_cache: true
+  after_create_commit :check_promote_buyer
+
+  def check_promote_buyer
+    return unless promote_buyer
+    self.promote_buyer.update state: 'used'
+  end
 
 end unless RailsTrade.config.disabled_models.include?('OrderPromote')
