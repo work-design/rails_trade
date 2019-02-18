9944f7d1b6760a3aa20e27e357b0d2a5a646bdf3
qinmingyuan <mingyuan0715@foxmail.com>
2019-02-18 10:55:46 +0800

    remove ship migration

--- db/migrate/20150514135720_rails_trade_order.rb
+++ db/migrate/20150514135720_rails_trade_order.rb
@@ -1,5 +1,4 @@
 class RailsTradeOrder < ActiveRecord::Migration[5.2]
-
   def change
 
     create_table :orders do |t|
@@ -50,28 +49,5 @@ class RailsTradeOrder < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
-    create_table :areas do |t|
-      t.string :nation, default: ''
-      t.string :province, default: ''
-      t.string :city, default: ''
-      t.string :district, default: ''
-      t.boolean :published, default: true
-      t.boolean :popular, default: false
-      t.timestamps
-    end
-
-    create_table :addresses do |t|
-      t.references :area
-      t.references :buyer, polymorphic: true
-      t.string :kind
-      t.timestamps
-    end
-
-    create_table :shipments do |t|
-      t.references :buyer, polymorphic: true
-      t.references :address
-    end
-
   end
-
 end
