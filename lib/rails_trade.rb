1ffe5e322bbeb3c2ded9af1d2d5efab9d49399df
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-20 10:42:07 +0800

    rails trade

--- lib/rails_trade.rb
+++ lib/rails_trade.rb
@@ -1,12 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_com'
 require 'rails_auth'
 require 'rails_role'
 require 'rails_data'
 require 'rails_audit'
 
-require 'rails_trade/engine'
-require 'rails_trade/config'
-
-module RailsTrade
-
-end
+require_relative 'rails_trade/engine'
+require_relative 'rails_trade/config'
