0142b8b7f05100db4f85ee7479b35e0fbf477e2b
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-23 16:41:33 +0800

    fix promote chage

--- app/models/rails_trade/promote_charge/final_promote_charge.rb
+++ app/models/rails_trade/promote_charge/final_promote_charge.rb
@@ -1,7 +1,7 @@
 module RailsTrade::PromoteCharge::FinalPromoteCharge
 
   def final_price(amount = nil)
-    parameter.round(2)
+    -(amount - parameter.round(2))
   end
 
 end
--- app/models/rails_trade/promote_charge/minus_promote_charge.rb
+++ app/models/rails_trade/promote_charge/minus_promote_charge.rb
@@ -1,7 +1,7 @@
 module RailsTrade::PromoteCharge::MinusPromoteCharge
 
   def final_price(amount)
-    (amount - parameter.abs).round(2)
+    - parameter.abs.round(2)
   end
 
 end
