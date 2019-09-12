49b8bf1aa7099a4fc3e25fec0461ba696baa8635
qinmingyuan <mingyuan0715@foxmail.com>
2019-09-12 09:56:00 +0800

    fix my cart

--- app/controllers/rails_trade/my_cart.rb
+++ app/controllers/rails_trade/my_cart.rb
@@ -6,11 +6,10 @@ module RailsTrade::MyCart
   
   def current_cart
     return @current_cart if defined? @current_cart
-    
     if current_user
       @current_cart = current_user.carts.default_where(default_params).find_or_create_by(default: true)
     else
-      @current_cart = Cart.find_or_create_by(default_params.merge(session_id: session.id))
+      @current_cart = Cart.find_or_create_by(default_form_params.merge(session_id: session.id))
     end
   end
   
