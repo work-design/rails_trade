ac6c9282609d687dad4dd2a55430561aa601478e
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-11 14:23:39 +0800

    clear require

--- lib/rails_trade.rb
+++ lib/rails_trade.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
-require 'rails_com'
-require 'rails_auth'
-require 'rails_role'
-require 'rails_data'
-require 'rails_audit'
-
-require_relative 'rails_trade/engine'
-require_relative 'rails_trade/config'
-
-require_relative 'rails_trade/apple_pay'
+require 'rails_trade/engine'
+require 'rails_trade/config'
+require 'rails_trade/apple_pay'
