9e80bf078297b30c6c4e1a9d2df61e12623c2dbd
qinmingyuan <mingyuan0715@foxmail.com>
2020-04-04 03:22:54 +0800

    code style

--- app/models/rails_trade/payment_type/alipay.rb
+++ app/models/rails_trade/payment_type/alipay.rb
@@ -1,7 +1,11 @@
 module RailsTrade::PaymentType::Alipay
 
   def alipay_prepay
-    Alipay2::Service.trade_app_pay_params(subject: self.subject, out_trade_no: self.uuid, total_amount: self.amount.to_s)
+    Alipay2::Service.trade_app_pay_params(
+      subject: self.subject,
+      out_trade_no: self.uuid,
+      total_amount: self.amount.to_s
+    )
   end
 
   def alipay_prepay_url(return_url: url_helpers.wait_my_order_url(self.id), notify_url: url_helpers.alipay_notify_payments_url)
