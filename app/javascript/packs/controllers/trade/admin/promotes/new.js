dd557522704297cbdcfb577e786909701575b5ab
qinmingyuan <mingyuan0715@foxmail.com>
2020-04-21 00:16:03 +0800

    reduce js files

--- app/javascript/packs/controllers/trade/admin/promotes/new.js
+++
@@ -1,5 +0,0 @@
-$('#promote_type').dropdown();
-$('#promote_scope').dropdown();
-$('#promote_extra').dropdown();
-$('#promote_deal_type').dropdown({placeholder: false});
-$('#promote_metering').dropdown();
