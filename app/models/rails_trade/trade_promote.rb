d40b1f3872512871d7d1f6d8794012dfaaab014e
qinmingyuan <mingyuan0715@foxmail.com>
2020-04-29 17:39:27 +0800

    moved scope in trade promote

--- app/models/rails_trade/trade_promote.rb
+++ app/models/rails_trade/trade_promote.rb
@@ -2,27 +2,21 @@ module RailsTrade::TradePromote
   extend ActiveSupport::Concern
   included do
     attribute :sequence, :integer
-    attribute :scope, :string
     attribute :original_amount, :decimal, comment: '初始价格'
     attribute :based_amount, :decimal, precision: 10, scale: 2, default: 0, comment: '基于此价格计算，默认为 trade_item 的 amount，与sequence有关'
     attribute :computed_amount, :decimal, precision: 10, scale: 2, default: 0, comment: '计算出的价格'
     attribute :amount, :decimal, precision: 10, scale: 2, default: 0, comment: '默认等于 computed_amount，如果客服人员修改过价格后，则 amount 会发生变化'
     attribute :note, :string, comment: '备注'
-    
+
     belongs_to :trade, polymorphic: true, inverse_of: :trade_promotes
     belongs_to :trade_item, optional: true
     belongs_to :promote
     belongs_to :promote_charge
     belongs_to :promote_good
     belongs_to :promote_buyer, counter_cache: true, optional: true
-    
+
     validates :promote_id, uniqueness: { scope: [:trade_type, :trade_id, :trade_item_id] }
     validates :amount, presence: true
-    
-    enum scope: {
-      single: 'single',
-      overall: 'overall'
-    }
 
     after_initialize if: :new_record? do
       if trade_item
@@ -31,7 +25,6 @@ module RailsTrade::TradePromote
       if self.promote_good
         self.promote_id = self.promote_good.promote_id
         self.sequence = self.promote.sequence
-        self.scope = self.promote.scope
       end
     end
     after_update :sync_changed_amount, if: -> { saved_change_to_amount? }
@@ -39,15 +32,15 @@ module RailsTrade::TradePromote
   end
 
   def compute_amount
-    if single?
+    if trade_item
       value = trade_item.metering_attributes.fetch(promote.metering, 0)
       added_amount = trade_item.trade_promotes.select { |cp| cp.promote.sequence < self.promote.sequence }.sum(&->(o){ o.send(promote.metering) })
-      
+
       self.based_amount = value + added_amount
     else
       value = trade.metering_attributes.fetch(promote.metering, 0)
       added_amount = trade.trade_promotes.select { |cp| cp.promote.sequence < self.promote.sequence }.sum(&->(o){ o.send(promote.metering) })
-      
+
       self.based_amount = value + added_amount
     end
 
@@ -55,7 +48,7 @@ module RailsTrade::TradePromote
     self.amount = computed_amount if amount.zero?
     self
   end
-  
+
   def sync_changed_amount
     if amount >= 0 && amount_before_last_save >= 0
       _additional_amount = amount - amount_before_last_save
@@ -70,17 +63,17 @@ module RailsTrade::TradePromote
       _additional_amount = 0
       _reduced_amount = amount - amount_before_last_save
     end
-    
-    if overall?
-      trade.overall_additional_amount += _additional_amount
-      trade.overall_reduced_amount += _reduced_amount
-      trade.amount += (_additional_amount + _reduced_amount)
-      trade.save!
-    elsif single?
+
+    if trade_item
       trade_item.additional_amount += _additional_amount
       trade_item.reduced_amount += _reduced_amount
       trade_item.amount += (_additional_amount + _reduced_amount)
       trade_item.save!
+    else
+      trade.overall_additional_amount += _additional_amount
+      trade.overall_reduced_amount += _reduced_amount
+      trade.amount += (_additional_amount + _reduced_amount)
+      trade.save!
     end
   end
 
@@ -88,5 +81,5 @@ module RailsTrade::TradePromote
     return unless promote_buyer
     self.promote_buyer.update state: 'used'
   end
-  
+
 end
