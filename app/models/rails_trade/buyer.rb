73e40828a8e6bb5401069b7f4ccb37d39c3ddc1e
qinmingyuan <mingyuan0715@foxmail.com>
2020-05-12 22:57:26 +0800

    no more buyer logic

--- app/models/rails_trade/buyer.rb
+++ app/models/rails_trade/buyer.rb
@@ -2,18 +2,10 @@ module RailsTrade::Buyer
   extend ActiveSupport::Concern
 
   included do
-    attribute :name, :string
-
-    has_many :carts, as: :buyer, dependent: :destroy
-
     has_many :payment_references, as: :buyer, dependent: :destroy, autosave: true
     has_many :payment_methods, through: :payment_references, autosave: true
 
     scope :credited, -> { where(payment_strategy_id: self.credit_ids) }
-  end
-
-  def name_detail
-    "#{name} (#{id})"
   end
 
   def last_overdue_date
