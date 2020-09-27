9bce61415f4804c46a9d3f247645fba515a68d0e
qinmingyuan <mingyuan0715@foxmail.com>
2020-09-27 11:48:54 +0800

    code indent

--- app/models/rails_trade/payment_strategy.rb
+++ app/models/rails_trade/payment_strategy.rb
@@ -1,17 +1,18 @@
 module RailsTrade::PaymentStrategy
   extend ActiveSupport::Concern
+
   included do
     attribute :name, :string
     attribute :period, :integer, default: 0, comment: '可延期时间，单位天'
     attribute :strategy, :string
-    
+
     enum strategy: {
       # 预付全款
       # 预付定金
       # 后付
     }
   end
-  
+
   def credit?
     self.period.to_i > 0
   end
