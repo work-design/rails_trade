5fdae557bb29f7799daedb6d27009d4e912bc774
qinmingyuan <mingyuan0715@foxmail.com>
2020-11-14 16:16:45 +0800

    rest amount

--- app/controllers/trade/my/carts_controller.rb
+++ app/controllers/trade/my/carts_controller.rb
@@ -2,7 +2,7 @@ class Trade::My::CartsController < Trade::My::BaseController
 
   def show
     q_params = {
-      status: 'init'
+      status: ['init', 'checked']
     }
     if params[:address_id].present?
       current_cart.update address_id: params[:address_id]
@@ -12,14 +12,14 @@ class Trade::My::CartsController < Trade::My::BaseController
   end
 
   def add
-    trade_item = current_cart.trade_items.find_or_initialize_by(good_id: params[:good_id], good_type: params[:good_type])
+    trade_item = current_cart.trade_items.where(status: ['init', 'checked']).find_by(good_id: params[:good_id], good_type: params[:good_type]) || current_cart.trade_items.build(good_id: params[:good_id], good_type: params[:good_type])
     if trade_item.persisted?
       params[:number] ||= 1
       trade_item.number += params[:number].to_i
     end
     trade_item.save
 
-    @trade_items = current_cart.trade_items.page(params[:page])
+    @trade_items = current_cart.trade_items.where(status: ['init', 'checked']).page(params[:page])
     @checked_ids = current_cart.trade_items.checked.pluck(:id)
   end
 
