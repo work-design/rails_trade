9b881f21f793e36b069cd1c83882d7002bf218e9
qinmingyuan <mingyuan0715@foxmail.com>
2020-12-26 19:31:41 +0800

    move js to sep file

--- app/javascript/packs/controllers/trade/admin/promote_charges/new.js
+++
@@ -1,7 +0,0 @@
-$('#promote_charge_type').dropdown({
-  onChange: function(value, text, $selectedItem){
-    var search_url = new URL('admin/promote_charges/options', location.origin);
-    search_url.searchParams.set('type', value);
-    Rails.ajax({url: search_url, type: 'GET', dataType: 'script'});
-  }
-});
