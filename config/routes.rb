1a2e2675386def0891768ec93ec9cecd4fd7771c
qinmingyuan <mingyuan0715@foxmail.com>
2021-02-08 00:49:23 +0800

    return success order list

--- config/routes.rb
+++ config/routes.rb
@@ -147,7 +147,7 @@ Rails.application.routes.draw do
         get 'cancel' => :edit_cancel
         put 'cancel' => :update_cancel
         put :refund
-        get :wait
+        get :success
       end
     end
     resources :payments
