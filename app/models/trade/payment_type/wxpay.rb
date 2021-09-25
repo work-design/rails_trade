e09ae78ed2e1a26304eb79e03bcc45601058132c
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-25 21:34:13 +0800

    only opt is app

--- app/models/trade/payment_type/wxpay.rb
+++ app/models/trade/payment_type/wxpay.rb
@@ -1,28 +1,28 @@
 module Trade
   module PaymentType::Wxpay
 
-    def wxpay_prepay(app:, notify_url: Rails.application.routes.url_for(controller: 'trade/payments', action: 'wxpay_notify'))
+    def wxpay_prepay(app)
       options = {
         mchid: app.mch_id,
         serial_no: app.serial_no,
         key: app.apiclient_key
       }
       params = {}
-      params.merge! wxpay_common_params(app: app, notify_url: notify_url)
+      params.merge! wxpay_common_params(app)
       params.merge! payer: { openid: user.oauth_users.find_by(appid: app.appid)&.uid }
       logger.debug "\e[35m  wxpay params: #{params}  \e[0m"
 
       ::WxPay::Api.invoke_unifiedorder params, options
     end
 
-    def h5_order(app:, notify_url: Rails.application.routes.url_for(controller: 'trade/payments', action: 'wxpay_notify'), payer_client_ip: '127.0.0.1')
+    def h5_order(app, payer_client_ip: '127.0.0.1')
       options = {
         mchid: app.mch_id,
         serial_no: app.serial_no,
         key: app.apiclient_key
       }
       params = {}
-      params.merge! wxpay_common_params(app: app, notify_url: notify_url)
+      params.merge! wxpay_common_params(app)
       params.merge! scene_info: { payer_client_ip: payer_client_ip, h5_info: { type: 'Wap' } }
 
       logger.debug "\e[35m  wxpay params: #{params}  \e[0m"
@@ -30,27 +30,27 @@ module Trade
       ::WxPay::Api.h5_order params, options
     end
 
-    def native_order(app:, notify_url: Rails.application.routes.url_for(controller: 'trade/payments', action: 'wxpay_notify'))
+    def native_order(app)
       options = {
         mchid: app.mch_id,
         serial_no: app.serial_no,
         key: app.apiclient_key
       }
       params = {}
-      params.merge! wxpay_common_params(app: app, notify_url: notify_url)
+      params.merge! wxpay_common_params(app)
 
       logger.debug "\e[35m  wxpay params: #{params}  \e[0m"
 
       ::WxPay::Api.native_order params, options
     end
 
-    def wxpay_common_params(app:, notify_url:)
+    def wxpay_common_params(app)
       {
         appid: app.appid,
         mchid: app.mch_id,
         description: "订单编号: #{self.uuid}",
         out_trade_no: self.uuid,
-        notify_url: notify_url,
+        notify_url: Rails.application.routes.url_for(host: app.host, controller: 'trade/payments', action: 'wxpay_notify'),
         amount: {
           total: (self.remaining_amount * 100).to_i,
           currency: 'CNY'
@@ -58,8 +58,8 @@ module Trade
       }
     end
 
-    def wxpay_order(app:, **opts)
-      prepay = wxpay_prepay(app: app, **opts)
+    def wxpay_order(app)
+      prepay = wxpay_prepay(app)
       options = {
         appid: app.appid,
         mchid: app.mch_id,
@@ -76,7 +76,7 @@ module Trade
       end
     end
 
-    def wxpay_result(app:)
+    def wxpay_result(app)
       #return self if self.payment_status == 'all_paid'
       options = {
         mchid: app.mch_id,
