ebdf278dc1a3fb9d6781d0e744e9683a96cfc893
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-10 22:00:55 +0800

    support pay in admin

--- app/models/trade/ordering/payment.rb
+++ app/models/trade/ordering/payment.rb
@@ -18,11 +18,11 @@ module Trade
     end
 
     def pending_payments
-      Payment.where.not(id: self.payment_orders.pluck(:payment_id)).where(payment_method_id: self.cart.payment_method_ids, state: ['init', 'part_checked'])
+      Payment.where.not(id: self.payment_orders.pluck(:payment_id)).where(payment_method_id: self.cart&.payment_method_ids, state: ['init', 'part_checked'])
     end
 
     def exists_payments
-      Payment.where.not(id: self.payment_orders.pluck(:payment_id)).exists?(payment_method_id: self.cart.payment_method_ids, state: ['init', 'part_checked'])
+      Payment.where.not(id: self.payment_orders.pluck(:payment_id)).exists?(payment_method_id: self.cart&.payment_method_ids, state: ['init', 'part_checked'])
     end
 
     def confirm_paid!
