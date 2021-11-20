1500ee12fe8c11cac5dd4f0e278321a199363f4f
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-20 22:31:32 +0800

    only destroy trade item

--- app/jobs/trade/trade_item_clean_job.rb
+++ app/jobs/trade/trade_item_clean_job.rb
@@ -2,7 +2,7 @@ module Trade
   class TradeItemCleanJob < ApplicationJob
 
     def perform(trade_item)
-      trade_item.destroy
+      trade_item.destroy if ['init', 'checked'].include?(trade_item.status)
     end
 
   end
