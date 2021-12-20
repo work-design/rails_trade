9aa73b509def4bc96cfa8007fe229ce077b27b31
qinmingyuan <mingyuan0715@foxmail.com>
2021-12-20 17:56:13 +0800

    privilege reorder

--- app/controllers/trade/admin/privileges_controller.rb
+++ app/controllers/trade/admin/privileges_controller.rb
@@ -1,11 +1,25 @@
 module Trade
   class Admin::PrivilegesController < Admin::BaseController
     before_action :set_card_template
-    before_action :set_privilege, only: [:show, :edit, :update, :destroy]
+    before_action :set_privilege, only: [:show, :edit, :update, :reorder, :destroy]
     before_action :set_new_privilege, only: [:new, :create]
 
     def index
       @privileges = @card_template.privileges.page(params[:page])
+    end
+
+    def reorder
+      sort_array = params[:sort_array].select { |i| i.integer? }
+
+      if params[:new_index] > params[:old_index]
+        prev_one = @privilege.class.find(sort_array[params[:new_index].to_i - 1])
+        @privilege.insert_at prev_one.position
+      else
+        next_ones = @privilege.class.find(sort_array[(params[:new_index].to_i + 1)..params[:old_index].to_i])
+        next_ones.each do |next_one|
+          next_one.insert_at @privilege.position
+        end
+      end
     end
 
     private
--- app/models/trade/model/privilege.rb
+++ app/models/trade/model/privilege.rb
@@ -7,11 +7,13 @@ module Trade
       attribute :amount, :integer, comment: '额度'
       attribute :price, :decimal, comment: '价格'
       attribute :code, :string
+      attribute :position, :integer
 
       belongs_to :card_template
       belongs_to :privileged, polymorphic: true, optional: true
 
       has_one_attached :logo
+      acts_as_list scope: :card_template_id
 
       #validates :code, uniqueness: { scope: :card_template_id }
     end
