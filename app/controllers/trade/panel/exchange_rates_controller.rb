d33ee716f563b390c845b2e58706be5e348a9bb6
qinmingyuan <mingyuan0715@foxmail.com>
2021-12-21 00:15:09 +0800

    simple exchage rates

--- app/controllers/trade/panel/exchange_rates_controller.rb
+++ app/controllers/trade/panel/exchange_rates_controller.rb
@@ -18,24 +18,6 @@ module Trade
       end
     end
 
-    def show
-    end
-
-    def edit
-    end
-
-    def update
-      @exchange_rate.assign_attributes(exchange_rate_params)
-
-      unless @exchange_rate.save
-        render :edit, locals: { model: @exchange_rate }, status: :unprocessable_entity
-      end
-    end
-
-    def destroy
-      @exchange_rate.destroy
-    end
-
     private
     def set_exchange_rate
       @exchange_rate = ExchangeRate.find(params[:id])
