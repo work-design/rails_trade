234599b79132c9266f12fe9a0e22e03e27251ddd
qinmingyuan <mingyuan0715@foxmail.com>
2022-01-03 16:45:51 +0800

    organ

--- app/models/trade/ext/organ.rb
+++ app/models/trade/ext/organ.rb
@@ -4,7 +4,7 @@ module Trade
 
     included do
       has_many :member_carts, class_name: 'Trade::Cart', foreign_key: :member_organ_id
-      has_many :trade_items, class_name: 'Trade::TradeItem', through: :member_carts
+      has_many :member_trade_items, class_name: 'Trade::TradeItem', through: :member_carts, source: :trade_items
     end
 
   end
