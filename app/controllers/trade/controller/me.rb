f892d6a116b2f6c08bdc489d6cc8ebc670a439cb
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-17 23:29:32 +0800

    support me cart

---
+++ app/controllers/trade/controller/me.rb
@@ -0,0 +1,24 @@
+module Trade
+  module Controller::Me
+    extend ActiveSupport::Concern
+
+    included do
+      helper_method :current_cart, :current_cart_count, :current_wallet
+    end
+
+    def current_cart
+      return @current_cart if @current_cart
+
+      options = {
+        member_id: current_member.id,
+        user_id: current_user.id,
+        organ_id: nil
+      }
+      @current_cart = current_user.carts.find_by(options) || current_user.carts.create(options)
+
+      logger.debug "\e[33m  Current Trade cart: #{@current_cart&.id}  \e[0m"
+      @current_cart
+    end
+
+  end
+end
