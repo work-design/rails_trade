9664b9a91cd2e579b9f3825b97f31be31ddd5328
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-07 22:58:33 +0800

    optional

--- app/models/trade/model/wallet.rb
+++ app/models/trade/model/wallet.rb
@@ -13,7 +13,7 @@ module Trade
       attribute :lock_version, :integer
       attribute :default, :boolean
 
-      belongs_to :organ, class_name: 'Org::Organ'
+      belongs_to :organ, class_name: 'Org::Organ', optional: true
       belongs_to :user, class_name: 'Auth::User'
       belongs_to :member, class_name: 'Org::Member', optional: true
       belongs_to :member_organ, class_name: 'Org::Organ', optional: true
