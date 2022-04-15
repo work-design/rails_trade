a60356eb8d21e61432abc853e4ea056d5e588013
qinmingyuan <mingyuan0715@foxmail.com>
2022-04-15 22:28:21 +0800

    promtoe gods

--- app/controllers/trade/admin/promote_good_types_controller.rb
+++ app/controllers/trade/admin/promote_good_types_controller.rb
@@ -1,3 +1,33 @@
-class Trade::Admin::PromoteGoodTypesController < Trade::Admin::BaseController
+module Trade
+  class Admin::PromoteGoodTypesController < Admin::BaseController
+    before_action :set_promote
+    before_action :set_new_promote_good, only: [:new, :create]
 
+    def index
+      @promote_goods = @promote.promote_goods.page(params[:page])
+    end
+
+    private
+    def set_promote
+      @promote = Promote.find params[:promote_id]
+    end
+
+    def set_promote_good
+      @promote_good = @promote.promote_goods.find params[:id]
+    end
+
+    def set_new_promote_good
+      @promote_good = @promote.promote_goods.build(type: 'Trade::PromoteGoodType')
+    end
+
+    def promote_good_params
+      params.fetch(:promote_good, {}).permit(
+        :effect_at,
+        :expire_at,
+        :good_type,
+        :good_id
+      )
+    end
+
+  end
 end
