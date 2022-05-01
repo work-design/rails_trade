45b22ef6cba77a1d70d02c1c08412f34adae4dfc
qinmingyuan <mingyuan0715@foxmail.com>
2022-05-01 23:50:17 +0800

    show item

--- app/controllers/trade/my/promote_goods_controller.rb
+++ app/controllers/trade/my/promote_goods_controller.rb
@@ -5,7 +5,7 @@ module Trade
       q_params = {}
       q_params.merge! params.permit(:state)
 
-      @promote_goods = current_cart.promote_goods.default_where(q_params).page(params[:page])
+      @promote_goods = current_cart.promote_goods.includes(:promote).default_where(q_params).page(params[:page])
     end
 
   end
