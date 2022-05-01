b324ca946157260b514fbade317569acfedb13b8
qinmingyuan <mingyuan0715@foxmail.com>
2022-05-01 20:53:21 +0800

    card change to purmary

--- app/models/trade/model/purchase.rb
+++ app/models/trade/model/purchase.rb
@@ -32,8 +32,10 @@ module Trade
       if trade_item.respond_to?(:maintain) && trade_item.maintain
         card = card_template.cards.find_or_initialize_by(client_type: trade_item.maintain.client_type, client_id: trade_item.maintain.client_id)
         card.agency_id = trade_item.maintain.agency_id
+        card.temporary = false
       else
         card = card_template.cards.find_or_initialize_by(user_id: trade_item.user_id, member_id: trade_item.member_id)
+        card.temporary = false
       end
       cp = card.card_purchases.build
       cp.trade_item = trade_item
