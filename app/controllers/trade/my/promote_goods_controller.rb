f06629ba8cd91989c109605dc9c454a66dce234c
qinmingyuan <mingyuan0715@foxmail.com>
2022-05-03 18:14:06 +0800

    show detail

--- app/controllers/trade/my/promote_goods_controller.rb
+++ app/controllers/trade/my/promote_goods_controller.rb
@@ -3,7 +3,9 @@ module Trade
     before_action :set_global_promotes, only: [:index]
 
     def index
-      q_params = {}
+      q_params = {
+        over_limit: false
+      }
       q_params.merge! params.permit(:state)
 
       @promote_goods = current_cart.promote_goods.includes(:promote).default_where(q_params).page(params[:page])
