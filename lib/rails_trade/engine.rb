0a7720c8105630fd189594a35d916ffa619863ea
qinmingyuan <mingyuan0715@foxmail.com>
2022-05-25 15:24:18 +0800

    moved unused resource

--- lib/rails_trade/engine.rb
+++ lib/rails_trade/engine.rb
@@ -23,8 +23,8 @@ module RailsTrade
     ]
 
     config.generators do |g|
-      g.helper = false
-      g.resource_route = false
+      g.helper false
+      g.resource_route false
       g.templates.unshift File.expand_path('lib/templates', RailsCom::Engine.root)
     end
 
