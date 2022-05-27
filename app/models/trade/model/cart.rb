ff29f6b68f61bc5e1a96c657e35f96ba6970b4b5
qinmingyuan <mingyuan0715@foxmail.com>
2022-05-27 14:21:49 +0800

    cart no need name

--- app/models/trade/model/cart.rb
+++ app/models/trade/model/cart.rb
@@ -16,7 +16,7 @@ module Trade
 
       belongs_to :organ, class_name: 'Org::Organ', optional: true
 
-      belongs_to :user, class_name: 'Auth::User'
+      belongs_to :user, class_name: 'Auth::User', optional: true
       belongs_to :member, class_name: 'Org::Member', optional: true
       belongs_to :member_organ, class_name: 'Org::Organ', optional: true
       belongs_to :address, class_name: 'Profiled::Address', optional: true
@@ -46,17 +46,13 @@ module Trade
       before_save :compute_promote, if: -> { original_amount_changed? }
     end
 
-    def name
-      member&.name || user.name
-    end
-
     def sync_member_organ
       self.member_organ_id = member.organ_id
-      self.user_id ||= member.user&.id
+      self.user ||= member.user
     end
 
     def set_myself
-      self.myself = (member.user&.id == user_id)
+      self.myself = (user.present? && member.user == user)
     end
 
     def sync_amount
