2f040bd0ebf34b27f4127f0100fbd2eda7fc3266
qinmingyuan <mingyuan0715@foxmail.com>
2022-05-27 14:01:17 +0800

    simple relateion

--- app/models/trade/ext/organ.rb
+++ app/models/trade/ext/organ.rb
@@ -4,7 +4,7 @@ module Trade
 
     included do
       has_many :member_carts, class_name: 'Trade::Cart', foreign_key: :member_organ_id
-      has_many :member_trade_items, class_name: 'Trade::TradeItem', through: :member_carts, source: :trade_items
+      has_many :member_trade_items, class_name: 'Trade::TradeItem', foreign_key: :member_organ_id
       has_many :member_orders, class_name: 'Trade::Order', foreign_key: :member_organ_id
       has_many :member_ordered_trade_items, class_name: 'Trade::TradeItem', through: :member_orders, source: :trade_items
     end
@@ -14,7 +14,7 @@ module Trade
       args.merge! 'produce_on' => options[:produce_on].to_date if options[:produce_on].present?
       args.merge! 'scene_id' => options[:scene_id].to_i if options[:scene_id].present?
 
-      trade_items.find(&->(i){ i.attributes.slice('good_type', 'good_id', 'aim', 'produce_on', 'scene_id').reject(&->(_, v){ v.blank? }) == args.stringify_keys })
+      member_trade_items.find(&->(i){ i.attributes.slice('good_type', 'good_id', 'aim', 'produce_on', 'scene_id').reject(&->(_, v){ v.blank? }) == args.stringify_keys })
     end
 
   end
