f569b9a6ae00459548955c830f28bd426244ec94
qinmingyuan <mingyuan0715@foxmail.com>
2022-05-27 10:58:55 +0800

    user id is optional for orga

--- app/models/trade/model/order.rb
+++ app/models/trade/model/order.rb
@@ -15,8 +15,9 @@ module Trade
       attribute :trade_items_count, :integer, default: 0
       attribute :paid_at, :datetime, index: true
 
-      belongs_to :user, class_name: 'Auth::User'
       belongs_to :organ, class_name: 'Org::Organ', optional: true
+
+      belongs_to :user, class_name: 'Auth::User', optional: true
       belongs_to :member, class_name: 'Org::Member', optional: true
       belongs_to :member_organ, class_name: 'Org::Organ', optional: true
       belongs_to :address, class_name: 'Profiled::Address', optional: true
