822b9e891ccd4e0d2524394f27879ab6e1cfb9c0
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-17 18:22:45 +0800

    add renta related resoces

---
+++ app/models/trade/ext/financial.rb
@@ -0,0 +1,15 @@
+module Trade
+  module Ext::Financial
+    extend ActiveSupport::Concern
+
+    included do
+      attribute :name, :string
+      attribute :price, :decimal
+      attribute :cost, :decimal
+      attribute :rentals_count, :integer, default: 0
+
+      has_many :rentals, ->{ where(rentable: true) }
+    end
+
+  end
+end
---
+++ app/models/trade/ext/rentable.rb
@@ -0,0 +1,23 @@
+module Trade
+  module Ext::Rentable
+    extend ActiveSupport::Concern
+
+    included do
+      attribute :rents_count, :integer, default: 0
+      attribute :rented, :boolean, default: false
+      attribute :rentable, :boolean, default: true
+
+      belongs_to :held_user, class_name: 'Auth::User', optional: true
+      belongs_to :held_organ, class_name: 'Org::Organ', optional: true
+      belongs_to :owned_user, class_name: 'Auth::User', optional: true
+      belongs_to :owned_organ, class_name: 'Org::Organ', optional: true
+
+      belongs_to :trade_item, class_name: 'Trade::TradeItem', optional: true
+
+      has_many :rents, class_name: 'Trade::TradeItem', as: :rentable
+
+      scope :ordered, -> { where.not(trade_item_id: nil) }
+    end
+
+  end
+end
