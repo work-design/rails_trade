aa70db86e43a12cf999f60ae1570bc599cf19e5a
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-18 14:37:48 +0800

    organ can has organ carts

--- app/models/trade/ext/organ.rb
+++ app/models/trade/ext/organ.rb
@@ -4,6 +4,7 @@ module Trade
 
     included do
       has_many :member_carts, class_name: 'Trade::Cart', foreign_key: :member_organ_id
+      has_many :organ_carts, ->{ where(member_id: nil, user_id: nil) }, class_name: 'Trade::Cart', foreign_key: :member_organ_id
       has_many :member_trade_items, class_name: 'Trade::TradeItem', foreign_key: :member_organ_id
       has_many :member_orders, class_name: 'Trade::Order', foreign_key: :member_organ_id
       has_many :member_ordered_trade_items, class_name: 'Trade::TradeItem', through: :member_orders, source: :trade_items
