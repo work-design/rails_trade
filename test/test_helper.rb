dd0428eb1347603cbd768751e4f6b59dbd5a9afe
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-05 14:40:43 +0800

    fix test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,11 +6,6 @@ require 'minitest/mock'
 ActiveRecord::Migrator.migrations_paths = [File.expand_path('../test/dummy/db/migrate', __dir__)]
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
-if defined?(FactoryBot)
-  FactoryBot.definition_file_paths << File.expand_path('factories', __dir__)
-end
-
 class ActiveSupport::TestCase
   self.file_fixture_path = File.expand_path('fixtures/files', __dir__)
-  include FactoryBot::Syntax::Methods if defined?(FactoryBot)
 end
