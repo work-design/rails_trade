4a08dd0718f7598070e38f673a07ac22a7ea17e2
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-12 09:57:32 +0800

    card and wallet maybe without user

--- app/models/trade/model/card.rb
+++ app/models/trade/model/card.rb
@@ -13,7 +13,7 @@ module Trade
       attribute :temporary, :boolean, default: false, comment: '在购物车勾选临时生效'
 
       belongs_to :organ, class_name: 'Org::Organ', optional: true
-      belongs_to :user, class_name: 'Auth::User'
+      belongs_to :user, class_name: 'Auth::User', optional: true
       belongs_to :member, class_name: 'Org::Member', optional: true
       belongs_to :member_organ, class_name: 'Org::Organ', optional: true
 
