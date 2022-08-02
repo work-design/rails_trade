1b906ae3201852e5b55e12e3d7cc5762511e0c2b
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-02 18:21:05 +0800

    support board scope

---
+++ app/controllers/trade/board/orders_controller.rb
@@ -0,0 +1,30 @@
+module Trade
+  class Board::OrdersController < My::OrdersController
+    before_action :set_order, only: [:show]
+
+    def index
+      q_params = {}
+      q_params.merge! params.permit(:id, :payment_type, :payment_status, :state)
+
+      @orders = current_user.orders.includes(:trade_items).default_where(q_params).order(id: :desc).page(params[:page])
+    end
+
+    private
+    def set_order
+      @order = current_user.orders.find(params[:id])
+    end
+
+    def order_params
+      params.fetch(:order, {}).permit(
+        :quantity,
+        :payment_id,
+        :payment_type,
+        :address_id,
+        :invoice_address_id,
+        :note,
+        trade_items_attributes: {}
+      )
+    end
+
+  end
+end
