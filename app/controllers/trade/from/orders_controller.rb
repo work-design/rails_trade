8cae502894f069667d2f58645edd662a5c7c08e4
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-04 17:15:32 +0800

    includes more

--- app/controllers/trade/from/orders_controller.rb
+++ app/controllers/trade/from/orders_controller.rb
@@ -7,7 +7,7 @@ module Trade
       q_params = {}
       q_params.merge! params.permit(:id, :payment_type, :payment_status, :state)
 
-      @orders = current_user.from_orders.includes(:trade_items, address: :area, from_address: :area).default_where(q_params).order(id: :desc).page(params[:page])
+      @orders = current_user.from_orders.includes(:trade_items, :payment_strategy, address: :area, from_address: :area).default_where(q_params).order(id: :desc).page(params[:page])
     end
 
     def new
