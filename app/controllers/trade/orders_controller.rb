028e26de7c1bbfdfd1ef6411a217f3b80c5a7e91
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-04 18:17:35 +0800

    redirect to current organ

--- app/controllers/trade/orders_controller.rb
+++ app/controllers/trade/orders_controller.rb
@@ -3,7 +3,11 @@ module Trade
     before_action :set_order, only: [:qrcode]
 
     def qrcode
-      redirect_to({ controller: 'trade/my/orders', action: 'show', id: params[:id], host: @order.organ.host }, allow_other_host: true)
+      if @order.generate_mode == 'by_from'
+        redirect_to({ controller: 'trade/board/orders', action: 'show', id: params[:id] })
+      else
+        redirect_to({ controller: 'trade/my/orders', action: 'show', id: params[:id], host: @order.organ.host }, allow_other_host: true)
+      end
     end
 
     private
