934bcad7cffca2c60c5d2cbcc9113d99b00a45ff
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-09 17:11:39 +0800

    current cart orgain id

--- app/models/trade/model/trade_item.rb
+++ app/models/trade/model/trade_item.rb
@@ -130,7 +130,7 @@ module Trade
     def sync_from_good
       return unless good
       self.good_name = good.name
-      self.organ_id = good.organ_id if good.respond_to? :organ_id
+      self.organ_id = (good.respond_to?(:organ_id) && good.organ_id) || current_cart&.organ_id
       self.produce_on = good.produce_on if good.respond_to? :produce_on
     end
 
