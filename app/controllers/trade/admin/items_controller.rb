6d629c5b541af3c8d7ddc2a981e8cf2c650e32d4
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-11 11:44:58 +0800

    rename items

--- app/controllers/trade/admin/items_controller.rb
+++ app/controllers/trade/admin/items_controller.rb
@@ -1,54 +1,54 @@
 module Trade
   class Admin::ItemsController < Admin::BaseController
-    before_action :set_trade_item, only: [:show, :update, :destroy, :actions, :carts]
+    before_action :set_item, only: [:show, :update, :destroy, :actions, :carts]
 
     def index
       q_params = {}
       q_params.merge! default_params
       q_params.merge! params.permit(:cart_id, :order_id, :good_type, :good_id, :aim, :address_id, :status)
 
-      @trade_items = Item.includes(:item_promotes).default_where(q_params).order(id: :desc).page(params[:page]).per(params[:per])
+      @items = Item.includes(:item_promotes).default_where(q_params).order(id: :desc).page(params[:page]).per(params[:per])
     end
 
     def carts
-      @carts = @trade_item.carts.includes(:user, :member)
+      @carts = @item.carts.includes(:user, :member)
     end
 
     def only
       unless params[:good_type] && params[:good_id]
-        redirect_back(fallback_location: admin_trade_items_url) and return
+        redirect_back(fallback_location: admin_items_url) and return
       end
 
       good = params[:good_type].safe_constantize&.find_by(id: params[:good_id])
       if good.respond_to?(:user_id)
         @user = good.user
         @buyer = @user.buyer
-        @trade_items = Item.where(good_type: params[:good_type], good_id: params[:good_id], user_id: good.user_id)
+        @items = Item.where(good_type: params[:good_type], good_id: params[:good_id], user_id: good.user_id)
       end
 
-      @trade_item = @trade_items.where(good_id: params[:good_id], good_type: params[:good_type]).first
-      if @trade_item.present?
+      @item = @items.where(good_id: params[:good_id], good_type: params[:good_type]).first
+      if @item.present?
         params[:quantity] ||= 0
-        @trade_item.checked = true
-        @trade_item.quantity = @trade_item.quantity + params[:quantity].to_i
-        @trade_item.save
+        @item.checked = true
+        @item.quantity = @item.quantity + params[:quantity].to_i
+        @item.save
       else
-        @trade_item = @trade_items.build(good_id: params[:good_id], good_type: params[:good_type], quantity: params[:quantity])
-        @trade_item.checked = true
-        @trade_item.save
+        @item = @items.build(good_id: params[:good_id], good_type: params[:good_type], quantity: params[:quantity])
+        @item.checked = true
+        @item.save
       end
 
-      @additions = @trade_item.total
+      @additions = @item.total
 
       render 'only'
     end
 
     def total
       if params[:add_id].present?
-        @add = @trade_items.find_by(id: params[:add_id])
+        @add = @items.find_by(id: params[:add_id])
         @add.update(checked: true)
       elsif params[:remove_id].present?
-        @remove = @trade_items.find_by(id: params[:remove_id])
+        @remove = @items.find_by(id: params[:remove_id])
         @remove.update(checked: false)
       end
     end
@@ -57,12 +57,12 @@ module Trade
     end
 
     private
-    def set_trade_item
-      @trade_item = Item.where(default_params).find params[:id]
+    def set_item
+      @item = Item.where(default_params).find params[:id]
     end
 
-    def trade_item_params
-      params.fetch(:trade_item, {}).permit(
+    def item_params
+      params.fetch(:item, {}).permit(
         :number,
         :current_cart_id
       )
