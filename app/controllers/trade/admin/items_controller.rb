ba6a7a6af13daf833975723bb7eda75f0a544126
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-30 17:18:37 +0800

    reduce n-1

--- app/controllers/trade/admin/items_controller.rb
+++ app/controllers/trade/admin/items_controller.rb
@@ -8,7 +8,7 @@ module Trade
       q_params.merge! default_params
       q_params.merge! params.permit(:cart_id, :order_id, :good_type, :good_id, :aim, :address_id, :status)
 
-      @items = Item.includes(:item_promotes).default_where(q_params).order(id: :desc).page(params[:page]).per(params[:per])
+      @items = Item.includes(:user, :item_promotes).default_where(q_params).order(id: :desc).page(params[:page]).per(params[:per])
     end
 
     def carts
