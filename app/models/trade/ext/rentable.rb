0de2a6012afe3a6f680674a19130df65b3d3d963
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-17 16:24:46 +0800

    add an  scope

--- app/models/trade/ext/rentable.rb
+++ app/models/trade/ext/rentable.rb
@@ -5,7 +5,6 @@ module Trade
     included do
       attribute :rents_count, :integer, default: 0
       attribute :rented, :boolean, default: false
-      attribute :rentable, :boolean, default: true
 
       belongs_to :held_user, class_name: 'Auth::User', optional: true
       belongs_to :held_organ, class_name: 'Org::Organ', optional: true
@@ -14,15 +13,15 @@ module Trade
       has_one :current_rent, ->(o){ where(user_id: o.held_user_id, member_organ_id: o.held_organ_id, finish_at: nil) }, class_name: 'Trade::Rent', as: :rentable
       has_one :last_rent, -> { order(start_at: :desc) }, class_name: 'Trade::Rent', as: :rentable
 
-      scope :saleable, -> { where(held_user_id: nil, held_organ_id: nil) }
-      scope :rentable, -> { where(rentable: true) }
+      scope :tradable, -> { where(held_user_id: nil, held_organ_id: nil) }
+      scope :traded, -> { where.not(held_user_id: nil).or(where.not(held_organ_id: nil)) }
       scope :rented, -> { where(rented: true) }
     end
 
     def do_rent(item)
       self.held_user_id = item.user_id
       self.held_organ_id = item.member_organ_id
-      self.rented = true
+      self.rented = true if item.aim_rent?
       #self.status = 'free' todo 考虑初始化状态
       self.rents.build(item_id: item.id)
       self
