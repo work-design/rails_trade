0d4f0273d0536d5f0ffb7943f8dd4f49a7af5d15
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-17 14:15:05 +0800

    simple helder user id

--- app/models/trade/ext/rentable.rb
+++ app/models/trade/ext/rentable.rb
@@ -9,14 +9,12 @@ module Trade
 
       belongs_to :held_user, class_name: 'Auth::User', optional: true
       belongs_to :held_organ, class_name: 'Org::Organ', optional: true
-      belongs_to :owned_user, class_name: 'Auth::User', optional: true
-      belongs_to :owned_organ, class_name: 'Org::Organ', optional: true
 
       has_many :rents, class_name: 'Trade::Rent', as: :rentable
       has_one :current_rent, ->(o){ where(user_id: o.held_user_id, member_organ_id: o.held_organ_id, finish_at: nil) }, class_name: 'Trade::Rent', as: :rentable
       has_one :last_rent, -> { order(start_at: :desc) }, class_name: 'Trade::Rent', as: :rentable
 
-      scope :saleable, -> { where(owned_user_id: nil, owned_organ_id: nil) }
+      scope :saleable, -> { where(held_user_id: nil, held_organ_id: nil) }
       scope :rentable, -> { where(rentable: true) }
       scope :rented, -> { where(rented: true) }
     end
@@ -25,6 +23,7 @@ module Trade
       self.held_user_id = item.user_id
       self.held_organ_id = item.member_organ_id
       self.rented = true
+      #self.status = 'free' todo 考虑初始化状态
       self.rents.build(item_id: item.id)
       self
 
