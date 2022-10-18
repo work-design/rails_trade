b6d43abd2d61f1b798d814d3c75b75045565ec7f
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-18 11:58:13 +0800

    use before save

--- app/models/trade/model/item.rb
+++ app/models/trade/model/item.rb
@@ -131,8 +131,8 @@ module Trade
       before_save :compute_duration, if: -> { rent_finish_at.present? && rent_finish_at_changed? }
       before_save :compute_estimate_duration, if: -> { rent_estimate_finish_at.present? && rent_estimate_finish_at_changed? }
       before_save :set_rent_start, if: -> { aim_rent? && status_changed? && ['deliverable'].include?(status) }
+      before_save :compute_promotes!, if: -> { (saved_changes.keys & PROMOTE_COLUMNS).present? }
       after_create :clean_when_expired, if: -> { expire_at.present? }
-      after_save :compute_promotes!, if: -> { (saved_changes.keys & PROMOTE_COLUMNS).present? }
       after_save :sync_amount_to_current_cart, if: -> { current_cart_id.present? && (saved_changes.keys & ['amount', 'status']).present? && ['init', 'checked', 'trial'].include?(status) }
       after_destroy :order_pruned!
       after_destroy :sync_amount_to_current_cart, if: -> { current_cart_id.present? && ['checked', 'trial'].include?(status) }
