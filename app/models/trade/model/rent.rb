e279c614b39415915b4dfc16ef55ec465e7ce88e
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-31 15:42:34 +0800

    combine same logic

--- app/models/trade/model/rent.rb
+++ app/models/trade/model/rent.rb
@@ -17,7 +17,6 @@ module Trade
       belongs_to :good, polymorphic: true, optional: true
 
       before_validation :sync_from_rentable, if: -> { rentable_id_changed? && rentable_id.present? }
-      before_save :sync_duration, if: -> { (finish_at.present? || estimate_finish_at.present?) && (['finish_at', 'estimate_finish_at'] & changes.keys).present? }
       before_save :compute_amount, if: -> { duration_changed? && duration.to_i > 0 }
       before_save :compute_invest_amount, if: -> { amount_changed? }
       after_save :sync_rentable_state, if: -> { saved_change_to_finish_at? }
@@ -29,16 +28,6 @@ module Trade
       self.user_id = rentable.held_user_id
       self.member_id = rentable.held_member_id
       self.member_organ_id = rentable.held_organ_id
-    end
-
-    def sync_duration
-      if finish_at
-        r = finish_at - start_at
-      else
-        r = estimate_finish_at - start_at
-      end
-      x = ActiveSupport::Duration.build(r.round).in_all.stringify_keys!
-      self.duration = x[rent_promote.unit_code].ceil if rent_promote
     end
 
     def compute_invest_amount
