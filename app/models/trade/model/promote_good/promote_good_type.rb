90ba3cd4da7c8495f4f6727b429a39ed750ba480
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-31 09:50:32 +0800

    unify indent

--- app/models/trade/model/promote_good/promote_good_type.rb
+++ app/models/trade/model/promote_good/promote_good_type.rb
@@ -5,8 +5,8 @@ module Trade
     included do
       attribute :blacklists_count, :integer, default: 0
 
-      has_many :blacklists, ->(o){ where(good_type: o.good_type, status: 'unavailable') }, class_name: self.name, primary_key: :promote_id, foreign_key: :promote_id
-      belongs_to :master,  ->(o){ where(good_type: o.good_type, good_id: nil, status: 'available') }, class_name: self.name, foreign_key: :promote_id, primary_key: :promote_id, counter_cache: :blacklists_count, optional: true
+      has_many :blacklists, ->(o) { where(good_type: o.good_type, status: 'unavailable') }, class_name: self.name, primary_key: :promote_id, foreign_key: :promote_id
+      belongs_to :master,  ->(o) { where(good_type: o.good_type, good_id: nil, status: 'available') }, class_name: self.name, foreign_key: :promote_id, primary_key: :promote_id, counter_cache: :blacklists_count, optional: true
     end
 
   end
