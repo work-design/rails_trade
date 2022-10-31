40ccb3edfc4082ea4e3decbe07b4fefe5ba3fefa
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-31 17:00:51 +0800

    add rent unit

--- app/models/trade/ext/rentable.rb
+++ app/models/trade/ext/rentable.rb
@@ -3,9 +3,18 @@ module Trade
     extend ActiveSupport::Concern
 
     included do
+      enum :rent_unit, {
+        seconds: 'seconds',
+        minutes: 'minutes',
+        hours: 'hours',
+        days: 'days',
+        weeks: 'weeks',
+        months: 'months',
+        years: 'years'
+      }, _default: 'hours'
+
       has_many :rent_charges, class_name: 'Trade::RentCharge', as: :rentable
     end
 
   end
 end
-
