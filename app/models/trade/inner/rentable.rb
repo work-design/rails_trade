9577878632219520eb9f1516eb9b1679cde067a9
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-31 16:33:00 +0800

    extrace rent compute

--- app/models/trade/inner/rentable.rb
+++ app/models/trade/inner/rentable.rb
@@ -15,46 +15,48 @@ module Trade
       attribute :rent_start_at, :datetime
       attribute :rent_finish_at, :datetime
       attribute :rent_present_finish_at, :datetime
+      attribute :duration, :integer, default: 0, comment: '单位统一为：秒'
+      attribute :amount, :decimal
       attribute :rent_estimate_finish_at, :datetime
-      attribute :estimate_metering, :json, default: {}
+      attribute :estimate_duration, :json, default: {}
       attribute :estimate_amount, :json, default: {}
-      attribute :duration, :integer, default: 0
-
-      has_one :rent_promote, -> { where(metering: 'duration') }, class_name: 'Trade::Promote', through: :rent_promote_goods, source: :promote
 
       before_save :compute_duration, if: -> { rent_finish_at.present? && rent_finish_at_changed? }
       before_save :compute_estimate_duration, if: -> { rent_estimate_finish_at.present? && rent_estimate_finish_at_changed? }
     end
 
+    def unit_code
+
+    end
+
+    def rent_charge
+
+    end
+
     def compute_later(now = Time.current)
-      return unless rent_promote
       if rent_present_finish_at
         r = rent_present_finish_at.parts.difference(rent_start_at.parts)
 
-        if r.delete(TIME_UNIT[rent_promote.unit_code])
-          next_at = rent_present_finish_at + 1.public_send(rent_promote.unit_code)
+        if r.delete(TIME_UNIT[unit_code])
+          next_at = rent_present_finish_at + 1.public_send(unit_code)
         else
           next_at = rent_present_finish_at.change(rent_start_at.parts.slice(*r.keys))
         end
       else
-        next_at = rent_start_at + 1.public_send(rent_promote.unit_code)
+        next_at = rent_start_at + 1.public_send(unit_code)
       end
 
       if next_at <= now
-        next_at += 1.public_send(rent_promote.unit_code)
+        next_at += 1.public_send(unit_code)
       end
 
       ItemRentJob.set(wait_until: next_at).perform_later(self, next_at)
     end
 
-    def compute_amount
-      return unless rent_promote
-      results = rent_promote.compute_price(duration, **extra)
-      self.amount = results.sum
-    end
-
     def compute_duration(now = rent_finish_at)
       self.duration = do_compute_duration(now)
+      results = rent_charge.compute_price(estimate_duration, **extra)
+      self.amount = results.sum
     end
 
     def compute_present_duration!(next_at)
@@ -64,18 +66,16 @@ module Trade
     end
 
     def compute_estimate_duration
-      metering_hash = attributes.slice(*PROMOTE_COLUMNS)
-      metering_hash.merge! 'duration' => do_compute_duration(rent_estimate_finish_at)  # 注意 hash key 须为 string 类型
-      self.estimate_metering = metering_hash
-      self.do_compute_promotes(estimate_metering)
-      self.estimate_amount = self.sum_amount
+      self.estimate_duration = do_compute_duration(rent_estimate_finish_at)
+      results = rent_charge.compute_price(estimate_duration, **extra)
+      self.estimate_amount = results.sum
     end
 
     def do_compute_duration(end_at = Time.current)
-      return unless end_at && rent_promote
+      return unless end_at
       r = end_at - rent_start_at
       x = ActiveSupport::Duration.build(r.round).in_all.stringify_keys!
-      x[rent_promote.unit_code].ceil
+      x[unit_code].ceil
     end
 
   end
