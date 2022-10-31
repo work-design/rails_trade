67ae1db3b072ee8d38e4b785ac01434e42434386
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-31 17:33:26 +0800

    rent number use integer

--- app/models/trade/model/rent_charge.rb
+++ app/models/trade/model/rent_charge.rb
@@ -4,6 +4,11 @@ module Trade
     include Inner::Charge
 
     included do
+      attribute :min, :integer, default: 0
+      attribute :max, :integer, default: 9999999999
+      attribute :filter_min, :integer, default: 0
+      attribute :filter_max, :integer, default: 9999999999
+
       belongs_to :rentable, polymorphic: true
     end
 
