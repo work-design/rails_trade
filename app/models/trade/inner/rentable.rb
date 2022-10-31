03f468135377a99f047f7d95fea827d0e39f0ce1
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-31 10:54:25 +0800

    share more logic

---
+++ app/models/trade/inner/rentable.rb
@@ -0,0 +1,65 @@
+module Trade
+  module Inner::Charge
+    extend ActiveSupport::Concern
+
+    included do
+      attribute :rent_start_at, :datetime
+      attribute :rent_finish_at, :datetime
+      attribute :rent_present_finish_at, :datetime
+      attribute :rent_estimate_finish_at, :datetime
+      attribute :estimate_metering, :json, default: {}
+      attribute :estimate_amount, :json, default: {}
+
+      has_one :rent_promote_good, ->(o) { rent.available.where(good_id: [o.id, nil]) }, class_name: 'Trade::PromoteGood', foreign_key: :good_type, primary_key: :good_type
+      has_one :rent_promote, -> { where(metering: 'duration') }, class_name: 'Trade::Promote', through: :rent_promote_good, source: :promote
+    end
+
+    def compute_later(now = Time.current)
+      return unless rent_promote
+      if rent_present_finish_at
+        r = rent_present_finish_at.parts.difference(rent_start_at.parts)
+
+        if r.delete(TIME_UNIT[rent_promote.unit_code])
+          next_at = rent_present_finish_at + 1.public_send(rent_promote.unit_code)
+        else
+          next_at = rent_present_finish_at.change(rent_start_at.parts.slice(*r.keys))
+        end
+      else
+        next_at = rent_start_at + 1.public_send(rent_promote.unit_code)
+      end
+
+      if next_at <= now
+        next_at += 1.public_send(rent_promote.unit_code)
+      end
+
+      ItemRentJob.set(wait_until: next_at).perform_later(self, next_at)
+    end
+
+    def compute_duration(now = rent_finish_at)
+      self.duration = do_compute_duration(now)
+    end
+
+    def compute_present_duration!(next_at)
+      self.rent_present_finish_at = next_at
+      self.duration = do_compute_duration(rent_present_finish_at)
+      self.save!
+    end
+
+    def compute_estimate_duration
+      metering_hash = attributes.slice(*PROMOTE_COLUMNS)
+      metering_hash.merge! 'duration' => do_compute_duration(rent_estimate_finish_at)  # 注意 hash key 须为 string 类型
+      self.estimate_metering = metering_hash
+      self.do_compute_promotes(estimate_metering)
+      self.estimate_amount = self.sum_amount
+    end
+
+    def do_compute_duration(end_at = Time.current)
+      return unless end_at && rent_promote
+      r = end_at - rent_start_at
+      x = ActiveSupport::Duration.build(r.round).in_all.stringify_keys!
+      x[rent_promote.unit_code].ceil
+    end
+
+  end
+end
+
--- app/models/trade/model/item.rb
+++ app/models/trade/model/item.rb
@@ -38,12 +38,6 @@ module Trade
       attribute :fetch_start_at, :datetime
       attribute :fetch_finish_at, :datetime
       attribute :organ_ancestor_ids, :json, default: []
-      attribute :rent_start_at, :datetime
-      attribute :rent_finish_at, :datetime
-      attribute :rent_present_finish_at, :datetime
-      attribute :rent_estimate_finish_at, :datetime
-      attribute :estimate_metering, :json, default: {}
-      attribute :estimate_amount, :json, default: {}
 
       enum status: {
         init: 'init',
@@ -106,9 +100,6 @@ module Trade
 
       has_many :unavailable_promote_goods, ->(o) { unavailable.where(organ_id: o.organ_ancestor_ids, good_id: [o.good_id, nil], aim: o.aim) }, class_name: 'PromoteGood', foreign_key: :good_type, primary_key: :good_type
       has_many :available_promote_goods, ->(o) { effective.where(organ_id: o.organ_ancestor_ids, good_id: [o.good_id, nil], user_id: [o.user_id, nil], member_id: [o.member_id, nil], aim: o.aim) }, class_name: 'PromoteGood', foreign_key: :good_type, primary_key: :good_type
-
-      has_one :rent_promote_good, ->(o) { rent.available.where(good_id: [o.id, nil]) }, class_name: 'Trade::PromoteGood', foreign_key: :good_type, primary_key: :good_type
-      has_one :rent_promote, -> { where(metering: 'duration') }, class_name: 'Trade::Promote', through: :rent_promote_good, source: :promote
 
       has_one_attached :image
 
@@ -395,52 +386,6 @@ module Trade
 
     def clean_when_expired
       ItemCleanJob.set(wait_until: expire_at).perform_later(self)
-    end
-
-    def compute_later(now = Time.current)
-      return unless rent_promote
-      if rent_present_finish_at
-        r = rent_present_finish_at.parts.difference(rent_start_at.parts)
-
-        if r.delete(TIME_UNIT[rent_promote.unit_code])
-          next_at = rent_present_finish_at + 1.public_send(rent_promote.unit_code)
-        else
-          next_at = rent_present_finish_at.change(rent_start_at.parts.slice(*r.keys))
-        end
-      else
-        next_at = rent_start_at + 1.public_send(rent_promote.unit_code)
-      end
-
-      if next_at <= now
-        next_at += 1.public_send(rent_promote.unit_code)
-      end
-
-      ItemRentJob.set(wait_until: next_at).perform_later(self, next_at)
-    end
-
-    def compute_duration(now = rent_finish_at)
-      self.duration = do_compute_duration(now)
-    end
-
-    def compute_present_duration!(next_at)
-      self.rent_present_finish_at = next_at
-      self.duration = do_compute_duration(rent_present_finish_at)
-      self.save!
-    end
-
-    def compute_estimate_duration
-      metering_hash = attributes.slice(*PROMOTE_COLUMNS)
-      metering_hash.merge! 'duration' => do_compute_duration(rent_estimate_finish_at)  # 注意 hash key 须为 string 类型
-      self.estimate_metering = metering_hash
-      self.do_compute_promotes(estimate_metering)
-      self.estimate_amount = self.sum_amount
-    end
-
-    def do_compute_duration(end_at = Time.current)
-      return unless end_at && rent_promote
-      r = end_at - rent_start_at
-      x = ActiveSupport::Duration.build(r.round).in_all.stringify_keys!
-      x[rent_promote.unit_code].ceil
     end
 
   end
