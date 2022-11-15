509a87097567f633e51785f0e23f7b464a41ce9a
qinmingyuan <mingyuan0715@foxmail.com>
2022-11-15 15:17:42 +0800

    fix error

--- app/controllers/trade/payments_controller.rb
+++ app/controllers/trade/payments_controller.rb
@@ -39,7 +39,7 @@ module Trade
 
     def wxpay_notify
       encrypted_params = JSON.parse(request.body.read)
-      notify_params = WxPay::Cipher.decrypt_notice encrypted_params['resource'], key: current_payee.key_v3
+      notify_params = WxPay::Cipher.decrypt_notice encrypted_params['resource'], key: current_payee.payee.key_v3
       #logger.debug WxPay::Sign.verify?(notify_params, key: current_payee.key)
       if notify_params['out_trade_no'].start_with?('PAY')
         @payment = Payment.find_by(payment_uuid: notify_params['out_trade_no'])
@@ -47,6 +47,7 @@ module Trade
         @order = Order.find_by(uuid: notify_params['out_trade_no'])
         @payment = @order.payments.build type: 'Trade::WxpayPayment', payment_uuid: notify_params['transaction_id'], total_amount: notify_params.dig('amount', 'total').to_i / 100.0
         @payment.checked_amount = @payment.total_amount
+        @payment.app_payee = current_payee
         @payment.organ_id = @order.organ_id
       end
       @payment.confirm(notify_params)
