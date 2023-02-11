563ba61536ae71568710ef16dd2d7f168b9e99b7
qinmingyuan <mingyuan0715@foxmail.com>
2023-02-11 21:57:16 +0800

    payment order

--- app/controllers/trade/in/payments_controller.rb
+++ app/controllers/trade/in/payments_controller.rb
@@ -1,6 +1,10 @@
 module Trade
   class In::PaymentsController < My::PaymentsController
 
+    private
+    def set_order
+      @order = current_organ.member_orders.find params[:order_id]
+    end
   end
 end
 
