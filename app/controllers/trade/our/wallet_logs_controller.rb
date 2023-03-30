e57524338679bba71804de906a02963a8863bd2f
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-30 16:20:56 +0800

    wallet logs

---
+++ app/controllers/trade/our/wallet_logs_controller.rb
@@ -0,0 +1,11 @@
+module Trade
+  class Our::WalletLogsController < My::WalletLogsController
+    before_action :set_wallet
+
+    private
+    def set_wallet
+      @wallet = current_user.wallets.find params[:wallet_id]
+    end
+
+  end
+end
