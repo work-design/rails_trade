5fa36e02cfc0377a65242345fdc06f747f13e20f
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-31 15:49:09 +0800

    record with partner pay

--- app/models/trade/model/payment/wxpay_payment.rb
+++ app/models/trade/model/payment/wxpay_payment.rb
@@ -83,8 +83,8 @@ module Trade
       self.notified_at = params['success_time']
       self.pay_status = params['trade_state']
       self.verified = true if self.pay_status == 'SUCCESS'
-      self.buyer_identifier = params.dig('payer', 'openid')
-      self.seller_identifier = params['mchid']
+      self.buyer_identifier = params.dig('payer', 'openid') || params.dig('payer', 'sub_openid')
+      self.seller_identifier = params['mchid'] || params['sub_mchid']
       self.buyer_bank = params['bank_type']
       self.total_amount = params.dig('amount', 'total').to_i / 100.0
       self.extra = params
