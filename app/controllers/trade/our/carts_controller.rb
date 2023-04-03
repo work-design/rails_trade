3b5947089e1562af7c47f4645e23cb6383113c92
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-03 23:29:56 +0800

    fix our carts

--- app/controllers/trade/our/carts_controller.rb
+++ app/controllers/trade/our/carts_controller.rb
@@ -4,27 +4,8 @@ module Trade
       skip_before_action :require_client, only: [:list]
     end
 
-    def show
-      q_params = {
-        status: ['init', 'checked']
-      }
-      if params[:address_id].present?
-        current_cart.update address_id: params[:address_id]
-      end
-
-      @items = current_cart.items.includes(produce_plan: :scene).default_where(q_params).order(id: :asc).page(params[:page])
-      @checked_ids = current_cart.items.default_where(q_params).unscope(where: :status).status_checked.pluck(:id)
-    end
-
     def addresses
-      @addresses = current_user.addresses.order(id: :asc)
-    end
-
-    def list
-      @members = current_user.members.includes(:organ)
-    end
-
-    def promote
+      @addresses = current_client.addresses.order(id: :asc)
     end
 
     private
