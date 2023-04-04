70870572393f8e4098a40641b07ea6d02b20f4c8
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-04 20:30:18 +0800

    client

--- app/controllers/trade/our/items_controller.rb
+++ app/controllers/trade/our/items_controller.rb
@@ -9,17 +9,15 @@ module Trade
 
     private
     def set_new_item
-      options = {
-        user_id: current_user.id,
-        member_id: current_member.id
-      }
+      options = {}
+      options.merge! client_params
       options.merge! params.permit(:good_type, :good_id, :aim, :number, :produce_on, :scene_id, :fetch_oneself)
 
       @item = Item.new(**options.to_h.symbolize_keys)
     end
 
     def set_item
-      @item = current_member.items.find(params[:id])
+      @item = current_client.items.find(params[:id])
     end
 
     def item_params
