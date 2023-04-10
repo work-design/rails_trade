ba12ee2dc3745cba86d5328f982157f4e6058f8b
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-10 21:28:17 +0800

    carts

---
+++ app/controllers/trade/mem/carts_controller.rb
@@ -0,0 +1,20 @@
+module Trade
+  class Mem::CartsController < My::CartsController
+    if whether_filter :require_client
+      skip_before_action :require_client, only: [:list]
+    end
+
+    def addresses
+      @addresses = current_client.addresses.order(id: :asc)
+    end
+
+    private
+    def cart_params
+      params.fetch(:cart, {}).permit(
+        :address_id,
+        :auto
+      )
+    end
+
+  end
+end
