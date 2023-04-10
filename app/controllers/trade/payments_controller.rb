5576149c98b5c032a88bd0c4936698895dbf733f
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-10 14:57:09 +0800

    add params mchid

--- app/controllers/trade/payments_controller.rb
+++ app/controllers/trade/payments_controller.rb
@@ -39,7 +39,8 @@ module Trade
 
     def wxpay_notify
       encrypted_params = JSON.parse(request.body.read)
-      notify_params = WxPay::Cipher.decrypt_notice encrypted_params['resource'], key: current_payee.payee.key_v3
+      payee = Wechat::Payee.find_by mch_id: params[:mch_id]
+      notify_params = WxPay::Cipher.decrypt_notice encrypted_params['resource'], key: payee.key_v3
       logger.debug "\e[35m  #{notify_params}  \e[0m"
 
       if notify_params['out_trade_no'].start_with?('PAY')
