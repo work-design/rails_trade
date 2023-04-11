787b053b2a27c74088a9a632fe6f9f248f15c5ea
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-11 11:19:35 +0800

    cart

--- app/controllers/trade/our/carts_controller.rb
+++ app/controllers/trade/our/carts_controller.rb
@@ -9,6 +9,14 @@ module Trade
     end
 
     private
+    def set_cart
+      options = {}
+      options.merge! default_form_params
+      options.merge! member_organ_id: current_client.organ_id
+
+      @cart = Cart.where(options).find params[:id]
+    end
+
     def cart_params
       params.fetch(:cart, {}).permit(
         :address_id,
