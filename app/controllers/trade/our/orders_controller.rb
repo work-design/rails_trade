5ea8e1abe2c1aeba8841f2f1de753e03dbfbf521
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-18 22:50:33 +0800

    payee

--- app/controllers/trade/our/orders_controller.rb
+++ app/controllers/trade/our/orders_controller.rb
@@ -50,20 +50,5 @@ module Trade
       p
     end
 
-    def current_payee
-      return @current_payee if defined?(@current_payee)
-
-      if params[:appid]
-        @current_payee = current_organ_domain.app_payees.find_by(appid: params[:appid])
-      elsif current_wechat_app
-        @current_payee = current_wechat_app.app_payees.take
-      else
-        @current_payee = current_organ_domain.app_payees.take
-      end
-
-      logger.debug "\e[35m  Current Payee: #{@current_payee&.id}  \e[0m"
-      @current_payee
-    end
-
   end
 end
