b77b9f20f9f70586ae89761ee0ab4bb9431bfbd9
qinmingyuan <mingyuan0715@foxmail.com>
2023-05-01 00:24:37 +0800

    card logic works

--- app/models/trade/model/card_purchase.rb
+++ app/models/trade/model/card_purchase.rb
@@ -27,35 +27,31 @@ module Trade
       }
 
       before_create :sync_from_card
-      after_save :sync_to_card, if: -> { (saved_changes.keys & ['years', 'months', 'days']).present? }
-      after_destroy :prune_to_card
+      after_save :sync_to_card!, if: -> { (saved_changes.keys & ['years', 'months', 'days', 'last_expire_at']).present? }
+      after_destroy :prune_to_card!
     end
 
     def duration
       years.years + months.months + days.days
     end
 
-    def last_expire_on(today = Date.today)
-      if last_expire_at && last_expire_at.to_date > today
-        last_expire_at.to_date
-      else
-        today
-      end
-    end
-
     def sync_from_card
-      self.last_expire_at = card.expire_at
+      if card.expire_at && card.expire_at.to_date > Date.today
+        self.last_expire_at = card.expire_at
+      else
+        self.last_expire_at = Date.today.end_of_day
+      end
       self.years = purchase.years
       self.months = purchase.months
       self.days = purchase.days
     end
 
-    def sync_to_card
-      card.expire_at = self.last_expire_on.since(duration).end_of_day
+    def sync_to_card!
+      card.expire_at = self.last_expire_at.since(duration).end_of_day
       card.save!
     end
 
-    def prune_to_card
+    def prune_to_card!
       card.expire_at = self.last_expire_at
       card.save!
     end
