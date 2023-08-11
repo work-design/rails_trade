bd0f7fd8a86baf7f4b73560d0190b47cd3b5fbc0
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-11 14:51:57 +0800

    simple notification

--- app/models/trade/model/item.rb
+++ app/models/trade/model/item.rb
@@ -362,17 +362,6 @@ module Trade
       self.save(*args)
     end
 
-    def to_notice
-      to_notification(
-        user: user,
-        title: '您的订单已准备好',
-        body: '您的订单将按时到达配送点',
-        link: Rails.application.routes.url_for(controller: 'trade/board/orders', action: 'show', id: order_id),
-        verbose: true,
-        organ_id: organ_id
-      )
-    end
-
     def order_work
       return unless good
       case status
