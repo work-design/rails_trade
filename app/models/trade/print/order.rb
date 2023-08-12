532b25b530d8efc4e70aa91165563f7ac0236978
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-12 16:11:36 +0800

    with tsql

--- app/models/trade/print/order.rb
+++ app/models/trade/print/order.rb
@@ -3,18 +3,21 @@ module Trade
 
     def print
       r = organ.device.print(
-        data: to_tspl,
-        msg_no: uuid
+        data: to_tspl
       )
     end
 
     def to_tspl
       url = Rails.application.routes.url_for(controller: 'trade/my/orders', action: 'show', id: id, host: organ.host)
       ts = BaseTspl.new
-      ts.bar(height: 20)
-      ts.qrcode(url, x: 20, y: 30, cell_width: 5)
-      ts.text(uuid, x: 320, scale: 2)
-      ts.middle_text('订单详情', x: 320)
+      ts.bar(y: 0, height: 20)
+      ts.right_qrcode(url, y: 30, cell_width: 5)
+      ts.text(uuid, x: 10, y:30)
+      ts.text(amount, x: 10)
+      ts.bar(height: 3, width: 200)
+      ts.text(address.contact_info, x: 10)
+      ts.text(address.content, x: 10)
+      ts.text("打包于 #{Date.today}", x: 10)
       ts.render
     end
   end
