cd4a359bb306d953abd35ded71e7755436b6d6ac
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-31 10:48:30 +0800

    default include item

--- app/models/trade/item.rb
+++ app/models/trade/item.rb
@@ -2,6 +2,7 @@ module Trade
   class Item < ApplicationRecord
     include Model::Item
     include Job::Ext::Jobbed
+    include Factory::Ext::Item if defined? RailsFactory
     include Crm::Ext::Maintainable if defined? RailsCrm
   end
 end
