90e56e19236f7d9d44c372e16eb1af140d652b0f
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-05 20:46:41 +0800

    add set cart

--- app/controllers/trade/in/items_controller.rb
+++ app/controllers/trade/in/items_controller.rb
@@ -1,7 +1,9 @@
 module Trade
   class In::ItemsController < Admin::ItemsController
+    before_action :set_cart, only: [:create, :update, :destroy, :toggle, :trial, :untrial]
     before_action :set_item, only: [:show]
     before_action :set_new_item, only: [:create, :cost]
+    before_action :set_cart_item, only: [:update, :destroy, :promote, :toggle, :finish]
 
     def cost
       @item.single_price = @item.good.cost
@@ -11,6 +13,19 @@ module Trade
     private
     def set_item
       @item = current_organ.organ_items.find params[:id]
+    end
+
+    def set_cart
+      if params[:current_cart_id].present?
+        @cart = Cart.find params[:current_cart_id]
+      elsif item_params[:current_cart_id].present?
+        @cart = Cart.find item_params[:current_cart_id]
+      else
+        options = { agent_id: current_member.id }
+        options.merge! default_form_params
+        options.merge! user_id: nil, member_id: nil, client_id: nil
+        @cart = Trade::Cart.where(options).find_or_create_by(good_type: params[:good_type], aim: params[:aim].presence || 'use')
+      end
     end
 
     def set_new_item
