cfb63bcd989f32169b04c0f0df5c343e191d33a9
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-06 11:21:25 +0800

    discard orders

--- app/models/trade/order.rb
+++ app/models/trade/order.rb
@@ -2,5 +2,6 @@ module Trade
   class Order < ApplicationRecord
     include Model::Order
     include Crm::Ext::Maintainable if defined? RailsCrm
+    include Auditor::Ext::Discard if defined? RailsAudit
   end
 end
