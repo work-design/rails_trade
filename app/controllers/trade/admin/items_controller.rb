921418147de5f8daeb47a585033afcdff0608184
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-20 14:52:34 +0800

    cart nav

--- app/controllers/trade/admin/items_controller.rb
+++ app/controllers/trade/admin/items_controller.rb
@@ -75,6 +75,19 @@ module Trade
     end
 
     private
+    def set_cart
+      if params[:current_cart_id].present?
+        @cart = Cart.find params[:current_cart_id]
+      elsif item_params[:current_cart_id].present?
+        @cart = Cart.find item_params[:current_cart_id]
+      else
+        options = { agent_id: current_member.id }
+        options.merge! default_form_params
+        options.merge! user_id: nil, member_id: nil, client_id: nil
+        @cart = Trade::Cart.where(options).find_or_create_by(good_type: params[:good_type], aim: params[:aim].presence || 'use')
+      end
+    end
+
     def set_item
       @item = Item.where(default_params).find params[:id]
     end
