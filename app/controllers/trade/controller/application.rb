9df07c2747b32a97376b11003c69a365a439e3cc
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-24 10:37:27 +0800

    simple the current cart logic

--- app/controllers/trade/controller/application.rb
+++ app/controllers/trade/controller/application.rb
@@ -3,30 +3,16 @@ module Trade
     extend ActiveSupport::Concern
 
     included do
-      helper_method :current_cart, :current_cart_count
-    end
-
-    def current_carts
-      return @current_carts if @current_carts
-
-      options = {}
-      options.merge! default_form_params
-      options.merge! client_params
-      @current_carts = Cart.where(options)
+      helper_method :current_cart_count
     end
 
     def current_cart
-      return @current_cart if @current_cart
-
-      if current_user
-        options = {}
-        options.merge! default_form_params
-        options.merge! member_id: current_member.id if current_member
-        @current_cart = current_user.carts.find_or_create_by(options)
-        @current_cart
+      return @current_cart if defined? @current_cart
+      if params[:current_cart_id].present?
+        @current_cart = Trade::Cart.find params[:current_cart_id]
+      elsif params.dig(:item, :current_cart_id).present?
+        @current_cart = Trade::Cart.find params.dig(:item, :current_cart_id)
       end
-      logger.debug "\e[33m  Current Trade cart: #{@current_cart&.id}  \e[0m"
-      @current_cart
     end
 
     def current_cart_count(good_type: 'Factory::Production')
