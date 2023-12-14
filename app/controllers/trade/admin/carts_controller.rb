d2637c8551605d90c3e883940c0c633d4c1500ce
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-14 16:51:24 +0800

    items

--- app/controllers/trade/admin/carts_controller.rb
+++ app/controllers/trade/admin/carts_controller.rb
@@ -9,7 +9,7 @@ module Trade
       q_params.merge! default_params
       q_params.merge! params.permit(:id)
 
-      @carts = Cart.includes(:user, :member, :member_organ, :payment_strategy, :items).where.not(user_id: nil).default_where(q_params).order(id: :desc).page(params[:page])
+      @carts = Cart.includes(:user, :member, :member_organ, :payment_strategy, :real_items, :organ_items).where.not(user_id: nil).default_where(q_params).order(id: :desc).page(params[:page])
     end
 
     def member_organ
