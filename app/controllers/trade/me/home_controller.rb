0697693ed628bf2b28d4f74bf35498f2b5d9a97a
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-27 12:40:51 +0800

    qrcode

--- app/controllers/trade/me/home_controller.rb
+++ app/controllers/trade/me/home_controller.rb
@@ -1,14 +1,20 @@
 module Trade
   class Me::HomeController < Me::BaseController
+    before_action :set_url, only: [:qrcode, :qrcode_file]
 
     def qrcode
     end
 
     def qrcode_file
       send_data(
-        QrcodeHelper.code_png(url_for(controller: 'trade/my/wxpay_payments', action: 'new', scope: 'snsapi_base', host: current_organ.host)),
+        QrcodeHelper.code_png(@url),
         filename: 'xx.png'
       )
+    end
+
+    priviate
+    def set_url
+      @url = url_for(controller: 'trade/my/wxpay_payments', action: 'new', scope: 'snsapi_base', host: current_organ.host)
     end
 
   end
