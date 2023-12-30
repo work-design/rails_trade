aa31817d8d4781e90bb874facc0dc9b163ef099b
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-30 20:12:47 +0800

    crm

--- app/controllers/trade/agent/payment_orders_controller.rb
+++ app/controllers/trade/agent/payment_orders_controller.rb
@@ -2,6 +2,13 @@ module Trade
   class Agent::PaymentOrdersController < Admin::PaymentOrdersController
     include Controller::Agent
 
+    def index
+      @payment_orders = @payment.payment_orders
+      if @payment.user_id
+        @contact = @payment.user.client_contacts.find_or_create_by(organ_id: current_organ.id)
+      end
+    end
+
   end
 end
 
