0887e51f0e649b0ecedaf9de2dfd13a8cadbc54a
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-30 20:50:47 +0800

    create maintain if not exists

--- app/controllers/trade/agent/payment_orders_controller.rb
+++ app/controllers/trade/agent/payment_orders_controller.rb
@@ -6,6 +6,7 @@ module Trade
       @payment_orders = @payment.payment_orders
       if @payment.user_id
         @contact = @payment.user.client_contacts.find_or_create_by(organ_id: current_organ.id)
+        @contact.maintains.find_or_create_by(member_id: current_member.id)
       end
     end
 
