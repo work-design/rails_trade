68284b26268b07891cfbceffa38caf69e3cedc6c
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-30 22:07:14 +0800

    link to maitin

--- app/controllers/trade/agent/payment_orders_controller.rb
+++ app/controllers/trade/agent/payment_orders_controller.rb
@@ -5,8 +5,8 @@ module Trade
     def index
       @payment_orders = @payment.payment_orders
       if @payment.user_id
-        @contact = @payment.user.client_contacts.find_or_create_by(organ_id: current_organ.id)
-        @contact.maintains.find_or_create_by(member_id: current_member.id)
+        contact = @payment.user.client_contacts.find_or_create_by(organ_id: current_organ.id)
+        @maintain = contact.maintains.find_or_create_by(member_id: current_member.id)
       end
     end
 
