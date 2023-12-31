eed9b4b5c04d80ab8a2e6974649697f027cafee0
qinmingyuan <mingyuan0715@foxmail.com>
2024-01-01 00:53:59 +0800

    cart

--- app/controllers/trade/controller/agent.rb
+++ app/controllers/trade/controller/agent.rb
@@ -15,7 +15,11 @@ module Trade
       @item = @cart.find_item(**options) || @cart.items.build(options)
       @item.status = 'checked'
       @item.assign_attributes params.permit(['station_id', 'desk_id', 'current_cart_id'] & Item.column_names)
-      @item.number = @item.number.to_i + (params[:number].presence || 1).to_i if @item.persisted?
+      if @item.persisted?
+        @item.number = @item.number.to_i + (params[:number].presence || 1).to_i
+      else
+        @item.number = params[:number].presence || 1
+      end
     end
 
     class_methods do
