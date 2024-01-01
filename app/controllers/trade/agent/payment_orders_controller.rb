0cf156d7edf20d2a452c27596ad9a793cd93dcc8
qinmingyuan <mingyuan0715@foxmail.com>
2024-01-01 11:55:25 +0800

    reload

--- app/controllers/trade/agent/payment_orders_controller.rb
+++ app/controllers/trade/agent/payment_orders_controller.rb
@@ -4,7 +4,7 @@ module Trade
 
     def index
       @payment_orders = @payment.payment_orders
-      if @payment.user_id
+      if @payment.user
         contact = @payment.user.client_contacts.find_or_create_by(organ_id: current_organ.id)
         @maintain = contact.maintains.find_or_create_by(member_id: current_member.id)
       end
