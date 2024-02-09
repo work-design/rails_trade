d0207dbde77439b098d8c8dcd48a163bb749b77a
qinmingyuan <mingyuan0715@foxmail.com>
2024-02-09 17:55:04 +0800

    refund

--- app/models/trade/model/refund_order.rb
+++ app/models/trade/model/refund_order.rb
@@ -18,36 +18,33 @@ module Trade
 
       has_many :refunds, foreign_key: :payment_id, primary_key: :payment_id
 
-      after_initialize :init_amount, if: -> { new_record? }
-      #after_update :checked_to_payment!, if: -> { state_confirmed? && (saved_changes.keys & ['state', 'payment_amount']).present? }
-      #after_update :unchecked_to_payment!, if: -> { state_init? && state_before_last_save == 'confirmed' }
+      after_update :sync_to_payment_and_order!, if: -> { state_refunding? && (saved_changes.keys & ['state', 'payment_amount', 'order_amount']).present? }
+      after_update :revert_to_payment_and_order!, if: -> { state_init? && state_before_last_save == 'refunding' }
       #after_destroy_commit :unchecked_to_order!
     end
 
-    def init_amount
+    def sync_to_payment_and_order!
+      payment.refuned_amount += self.payment_amount
 
-    end
-
-    def checked_to_payment!
-      payment.checked_amount += self.payment_amount
-      payment.save
-    end
-
-    def unchecked_to_payment!
-      payment.checked_amount -= self.payment_amount
-      payment.save
-    end
-
-    def update_order_received_amount
       order.refund_amount += self.order_amount
       order.unreceived_amount = order.amount - order.received_amount - order.refund_amount
+
+      self.class.transaction do
+        payment.save!
+        order.save!
+      end
     end
 
-    def unchecked_to_order!
-      return if order.blank?
-      order.received_amount -= self.order_amount
-      order.check_state
-      order.save
+    def revert_to_payment_and_order!
+      payment.refuned_amount -= self.payment_amount
+
+      order.refund_amount -= self.order_amount
+      order.unreceived_amount = order.amount - order.received_amount - order.refund_amount
+
+      self.class.transaction do
+        payment.save!
+        order.save!
+      end
     end
 
   end
