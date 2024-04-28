fb51bbc42de8d4d51bb6c5e78c17c37cd856ebbe
qinmingyuan <mingyuan0715@foxmail.com>
2024-04-29 00:00:03 +0800

    show good name

--- app/models/trade/print/order.rb
+++ app/models/trade/print/order.rb
@@ -9,11 +9,15 @@ module Trade
 
     def to_tspl
       url = Rails.application.routes.url_for(controller: 'trade/my/orders', action: 'show', id: id, host: organ.host)
+
       ts = BaseTspl.new
       ts.bar(y: 0, height: 20)
       ts.right_qrcode(url, y: 30, cell_width: 5)
       ts.text(uuid, x: 10, y:30)
-      ts.text(amount, x: 10)
+      items.limit(3).each do |item|
+        ts.text("#{item.good.name} x #{item.number}", x: 10)
+      end
+      ts.text(amount.to_money.format(html_wrap: false), x: 10)
       ts.bar(height: 3, width: 200)
       ts.text(serial_number, font: 'TSS32.BF2', x: 10)
       if address
