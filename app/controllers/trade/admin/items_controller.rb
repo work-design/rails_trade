4b5e0b6c7a1dcb70fdaef211f0c08e9b4165bb6a
qinmingyuan <mingyuan0715@foxmail.com>
2024-05-05 12:05:26 +0800

    should allwith order

--- app/controllers/trade/admin/items_controller.rb
+++ app/controllers/trade/admin/items_controller.rb
@@ -11,7 +11,7 @@ module Trade
       q_params.merge! default_params
       q_params.merge! params.permit(:cart_id, :order_id, :good_type, :good_id, :aim, :address_id, :status)
 
-      @items = Item.includes(:user, :item_promotes, :order).default_where(q_params).order(id: :desc).page(params[:page]).per(params[:per])
+      @items = Item.includes(:user, :item_promotes, :order).where.not(order_id: nil).default_where(q_params).order(id: :desc).page(params[:page]).per(params[:per])
     end
 
     def purchase
