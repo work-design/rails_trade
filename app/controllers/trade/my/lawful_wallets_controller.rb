33d973df3e6d67af13855386034b361cb4645dbb
qinmingyuan <mingyuan0715@foxmail.com>
2024-05-05 10:43:25 +0800

    cart for my

--- app/controllers/trade/my/lawful_wallets_controller.rb
+++ app/controllers/trade/my/lawful_wallets_controller.rb
@@ -2,6 +2,7 @@ module Trade
   class My::LawfulWalletsController < My::BaseController
     before_action :set_lawful_wallet, only: [:show, :edit, :update, :account]
     before_action :set_new_order, only: [:show]
+    before_action :set_cart, only: [:show]
 
     def account
     end
