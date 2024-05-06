e513471ab859756d9926648c8eb8115208dd3e5b
qinmingyuan <mingyuan0715@foxmail.com>
2024-05-06 22:49:23 +0800

    wallet

--- app/models/trade/wallet.rb
+++ app/models/trade/wallet.rb
@@ -2,5 +2,8 @@ module Trade
   class Wallet < ApplicationRecord
     include Model::Wallet
     include Crm::Ext::Maintainable if defined? RailsCrm
+    if defined? RailsAudit
+      include Auditor::Ext::Discard
+    end
   end
 end
