0a964e6caf581e645c5101b3922a62d1d9b5c1cc
qinmingyuan <mingyuan0715@foxmail.com>
2024-05-06 22:43:17 +0800

    reocrd destroy card

--- app/models/trade/card.rb
+++ app/models/trade/card.rb
@@ -2,5 +2,8 @@ module Trade
   class Card < ApplicationRecord
     include Model::Card
     include Crm::Ext::Maintainable if defined? RailsCrm
+    if defined? RailsAudit
+      include Auditor::Ext::Discard
+    end
   end
 end
