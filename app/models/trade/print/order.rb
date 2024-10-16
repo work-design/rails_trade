8d8188c685a2aeae1081914eaa7e41648d3221c9
qinmingyuan <mingyuan0715@foxmail.com>
2024-10-16 11:52:14 +0800

    note

--- app/models/trade/print/order.rb
+++ app/models/trade/print/order.rb
@@ -46,7 +46,7 @@ module Trade
     def to_cpcl
       cpcl = BaseCpcl.new
       cpcl.text serial_str
-      cpcl.right_qrcode(qrcode_show_url)
+      cpcl.right_qrcode(note, y: 20)
       cpcl.render
     end
 
