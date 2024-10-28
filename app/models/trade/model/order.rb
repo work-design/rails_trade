d800f3dcd259e2a00baa9506a450de339200c4e8
qinmingyuan <mingyuan0715@foxmail.com>
2024-10-29 00:29:50 +0800

    simple code logic

--- app/models/trade/model/order.rb
+++ app/models/trade/model/order.rb
@@ -101,7 +101,6 @@ module Trade
       after_save :confirm_refund!, if: -> { refunding? && saved_change_to_payment_status? }
       after_save :sync_to_unpaid_payment_orders, if: -> { (saved_changes.keys & ['overall_additional_amount', 'item_amount']).present? }
       after_create :sync_ordered_to_current_cart, if: -> { current_cart_id.present? }
-      after_create :set_not_fresh, if: -> { current_cart_id.present? }
       after_save_commit :lawful_wallet_pay, if: -> { pay_auto && saved_change_to_pay_auto? }
       after_save_commit :send_notice_after_commit, if: -> { saved_change_to_payment_status? }
     end
@@ -178,14 +177,7 @@ module Trade
       current_cart.with_lock do
         current_cart.compute_amount!
       end
-    end
-
-    def set_not_fresh
-      if current_cart_id
-        carts.where.not(id: current_cart_id).update_all(fresh: false)
-      else
-        carts.update_all(fresh: false)
-      end
+      carts.where.not(id: current_cart_id).update_all(fresh: false)
     end
 
     def sync_to_unpaid_payment_orders
