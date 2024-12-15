382bc6976cee4478f38a10a8d042c9cd02255648
qinmingyuan <mingyuan0715@foxmail.com>
2024-12-15 14:59:20 +0800

    fix error

--- app/controllers/trade/controller/in.rb
+++ app/controllers/trade/controller/in.rb
@@ -2,7 +2,7 @@ module Trade
   module Controller::In
     extend ActiveSupport::Concern
     include Controller::Application
-    include Org::Controller::In
+    include Org::Controller::In if defined? RailsOrg
 
     included do
       layout -> { turbo_frame_body? ? "frame/#{proper_layout}" : 'admin' }
--- app/models/trade/payment_order.rb
+++ app/models/trade/payment_order.rb
@@ -1,6 +1,6 @@
 module Trade
   class PaymentOrder < ApplicationRecord
     include Model::PaymentOrder
-    include Auditor::Ext::Audited
+    include Auditor::Ext::Audited if defined? RailsAudit
   end
 end
