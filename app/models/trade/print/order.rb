30102bee21917d40525f628f008022e8d4eb8327
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-02 15:51:30 +0800

    qrcode

--- app/models/trade/print/order.rb
+++ app/models/trade/print/order.rb
@@ -38,7 +38,7 @@ module Trade
 
     def to_tspl
       ts.bar(y: 0, height: 20)
-      ts.right_qrcode(qrcode_show_url, y: 30, cell_width: 5)
+      ts.qrcode_right(qrcode_show_url, y: 30, cell_width: 5)
       ts.text(serial_str, font: 'TSS32.BF2', x: 10, y:30)
       ts.bar(height: 3, width: 250)
       items.limit(3).each do |item|
