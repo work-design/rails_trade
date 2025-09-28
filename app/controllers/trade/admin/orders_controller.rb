99b61f6ab4f8ec53de4045fb1d54dbf38cf20878
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-28 15:54:01 +0800

    test with raw

--- app/controllers/trade/admin/orders_controller.rb
+++ app/controllers/trade/admin/orders_controller.rb
@@ -142,7 +142,7 @@ module Trade
     def print_data
       esc = BaseEsc.new
       data = @order.to_esc(esc)
-      render json: data.render_0x
+      render json: data.render_raw
     end
 
     def print
