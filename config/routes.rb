9dea843204893946021c6d3763616834d63ed041
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-13 09:45:06 +0800

    simple paywxpay

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
         match :payment_types, via: [:get, :post]
         post :payment_pending
         post :payment_confirm
-        match :payment_wxpay, via: [:get, :post]
         get :payment_frozen
         get :payment_type
         get 'cancel' => :edit_cancel
