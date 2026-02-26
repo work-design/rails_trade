3cae19515e7ce698bdca6649cb2d3504fe89ffd9
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-26 14:53:16 +0800

    allow code blank

--- app/models/trade/model/wallet_template.rb
+++ app/models/trade/model/wallet_template.rb
@@ -29,7 +29,7 @@ module Trade
 
       accepts_nested_attributes_for :advances
 
-      validates :code, uniqueness: { scope: :organ_id }
+      validates :code, uniqueness: { scope: :organ_id }, allow_blank: true
     end
 
     def existing_good_types
---
+++ app/models/wallet_template.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+WalletTemplate = Trade::WalletTemplate unless defined? WalletTemplate
