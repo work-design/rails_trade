4efe5e62bea9c66acea1c57fe58bf0787c72f188
qinmingyuan <mingyuan0715@foxmail.com>
2026-03-13 16:29:19 +0800

    defualt statis logic

--- app/models/trade/order_counter_cache.rb
+++ app/models/trade/order_counter_cache.rb
@@ -1,5 +1,15 @@
 module Trade
   class OrderCounterCache < ApplicationRecord
     include Statis::Ext::Config
+
+    attribute :state, :string
+    attribute :payment_status, :string
+
+    belongs_to :organ, class_name: 'Org::Organ', optional: true
+
+    def countable
+      Order
+    end
+
   end
 end
