5fcadcdb1d0f415c2556b03ee948c93970cb4acf
qinmingyuan <mingyuan0715@foxmail.com>
2026-03-20 18:19:34 +0800

    fix

--- app/controllers/trade/admin/payments_controller.rb
+++ app/controllers/trade/admin/payments_controller.rb
@@ -12,7 +12,7 @@ module Trade
     def index
       q_params = {}
       q_params.merge! default_params
-      q_params.merge! params.permit(:type, :state, :id, :buyer_identifier, :buyer_bank, :payment_uuid, 'buyer_name-like', 'payment_orders.state', 'orders.uuid')
+      q_params.merge! params.permit(:type, :state, :pay_state, :id, :buyer_identifier, :buyer_bank, :payment_uuid, 'buyer_name-like', 'payment_orders.state', 'orders.uuid')
 
       set_count(q_params)
       @payments = Payment.includes(:payment_method, :payment_orders).default_where(q_params).order(id: :desc).page(params[:page])
