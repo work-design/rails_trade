0f629383b04b04ea80f12bb4762b3a13f8735726
qinmingyuan <mingyuan0715@foxmail.com>
2026-03-22 07:02:27 +0800

    fix wallet goods

--- app/models/trade/model/wallet_good.rb
+++ app/models/trade/model/wallet_good.rb
@@ -10,10 +10,17 @@ module Trade
       belongs_to :wallet_template
 
       before_validation :sync_wallet_code, if: -> { wallet_template_id_changed? }
+      after_save_commit :change_wallet_price, if: -> { saved_change_to_ratio? }
     end
 
     def sync_wallet_code
       self.wallet_code = wallet_template.code
+    end
+
+    def change_wallet_price
+      Factory::Production.json_filter_any(:wallet_price, wallet_code).each do |good|
+        good.set_wallet_price! self
+      end
     end
 
   end
