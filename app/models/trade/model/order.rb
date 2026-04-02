69cbc064bc14a7b41dae707c254f29d8a3a01d82
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-02 13:51:51 +0800

    to check loigc

--- app/models/trade/model/order.rb
+++ app/models/trade/model/order.rb
@@ -245,7 +245,7 @@ module Trade
     end
 
     def can_pay?
-      ['unpaid', 'part_paid'].include?(self.payment_status) && ['init'].include?(self.state)
+      ['unpaid', 'to_check', 'part_paid'].include?(self.payment_status) && ['init'].include?(self.state)
     end
 
     def can_cancel?
