77e5a68b6c302aa22f5f3a21ee871c45e8604810
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-02 16:25:46 +0800

    fix

--- app/controllers/trade/my/carts_controller.rb
+++ app/controllers/trade/my/carts_controller.rb
@@ -87,7 +87,7 @@ module Trade
       if params[:role_id].present?
         @roles = Roled::Role.visible.where(id: params[:role_id])
       else
-        @roles = Roled::Role.visible
+        @roles = Roled::Role.visible.where.not(tip: nil)
       end
     end
 
