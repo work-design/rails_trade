4c9a6a4a59ae24319979d67950624528101e3ad3
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-06 18:31:15 +0800

    fix

--- app/models/trade/model/order.rb
+++ app/models/trade/model/order.rb
@@ -573,6 +573,13 @@ module Trade
         user_id: user_id,
         **options.slice(:wallet_id, :appid, :seller_identifier, :buyer_identifier, :payment_uuid)
       )
+
+      # 支付成功后，将用户信息赋予支付者
+      if user_id.blank? && p.respond_to?(:wechat_user)
+        self.user_id = p.wechat_user&.user_id
+        p.user_id = user_id
+      end
+
       p.assign_detail options
       p
     end
