bff6537a216494c4a2e30521d018610c9fcd665f
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-07 15:25:16 +0800

    lvel

--- app/controllers/trade/panel/orders_controller.rb
+++ app/controllers/trade/panel/orders_controller.rb
@@ -7,6 +7,7 @@ module Trade
       q_params.merge! params.permit(:id, :uuid, :user_id, :member_id, :payment_status, :state, :payment_type)
 
       @orders = Order.includes(:organ, :user, :member, :member_organ).default_where(q_params).order(id: :desc).page(params[:page]).per(params[:per])
+      @grouped_orders = @orders.group_by { |i| i.created_at.to_date }
     end
 
     def refund
