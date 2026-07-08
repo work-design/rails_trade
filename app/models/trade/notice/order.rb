95b8ffbca7cce057fdac35b13c62e0a062538f3a
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-08 16:46:25 +0800

    add more notice

--- app/models/trade/notice/order.rb
+++ app/models/trade/notice/order.rb
@@ -8,7 +8,8 @@ module Trade
         only: ['uuid', 'created_at', 'note']
       )
 
-      after_save_commit :to_provider_notice, if: -> { saved_change_to_payment_status? && payment_status == 'all_paid' }
+      after_save_commit :to_organ_notice, if: -> { saved_change_to_payment_status? && payment_status == 'all_paid' }
+      after_save_commit :to_provider_notice, if: -> { saved_change_to_state? && state == 'produced' }
     end
 
     def to_notice
@@ -24,6 +25,24 @@ module Trade
         verbose: true,
         organ_id: organ_id
       )
+    end
+
+    def to_organ_notice
+      return unless organ
+      organ.ancestral_members.where('notifiable_types ? :type', type: self.base_class_name).each do |member|
+        to_member_notice(
+          member: member,
+          title: "收到新订单 #{note}",
+          body: '您的订单将按时到达配送点',
+          link: Rails.app.routes.url_for(
+            controller: 'trade/admin/orders',
+            action: 'show',
+            id: id,
+            host: organ.admin_host
+          ),
+          verbose: true
+        )
+      end
     end
 
     def to_provider_notice
