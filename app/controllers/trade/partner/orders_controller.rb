f440feafd3a2eeffa8743a41de24e4e4a1d110d3
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-08 11:49:02 +0800

    organ ids

--- app/controllers/trade/partner/orders_controller.rb
+++ app/controllers/trade/partner/orders_controller.rb
@@ -3,5 +3,13 @@ module Trade
     include Org::Controller::Admin
     before_action :require_org_member
 
+    def index
+      q_params = { organ_id: current_organ.organ_ids }
+      q_params.merge! params.permit(:id, :uuid, :user_id, :member_id, :payment_status, :state, :payment_type)
+
+      @orders = Order.includes(:organ, :user, :member, :member_organ).default_where(q_params).order(id: :desc).page(params[:page]).per(params[:per])
+      @grouped_orders = @orders.group_by { |i| i.created_at.to_date }
+    end
+
   end
 end
