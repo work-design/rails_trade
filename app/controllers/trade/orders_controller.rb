dc6ddbad0115067e18f0427f2c322f78bcea63e2
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-11 11:41:57 +0800

    support redirect to partner admin

--- app/controllers/trade/orders_controller.rb
+++ app/controllers/trade/orders_controller.rb
@@ -7,6 +7,8 @@ module Trade
         redirect_to({ controller: 'trade/my/orders', action: 'show', id: params[:id], host: @order.organ.host }, allow_other_host: true)
       elsif current_user.members.pluck(:organ_id).include? @order.organ_id
         redirect_to({ controller: 'trade/admin/orders', action: 'show', id: @order.id, host: @order.organ.admin_host }, allow_other_host: true)
+      elsif current_user.members.pluck(:organ_id).include? @order.organ&.provider_id
+        redirect_to({ controller: 'trade/partner/orders', action: 'show', id: @order.id, host: @order.organ.provider.admin_host }, allow_other_host: true)
       elsif @order.generate_mode == 'by_from'
         redirect_to({ controller: 'trade/board/orders', action: 'show', id: params[:id] })
       elsif @order.can_pay?
